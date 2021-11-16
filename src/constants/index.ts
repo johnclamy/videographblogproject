24124de91eecd03a787ada7c012ca9c8f2412ddf
John_Lamy <johnclamy@gmail.com>
2021-11-16 22:02:45 +0000

    colour constants

---
+++ src/constants/index.ts
@@ -0,0 +1,39 @@
+import Color from '../types/Color'
+
+const color: Color = {
+  /* App primary colours */
+  black:        '#212121',
+  darkBlue:     '#303f9f',
+  blue:         '#3f51b5',
+  lightBlue:    '#c5cae9',
+  pink:         '#ff4081',
+  grey:         '#727272',
+  lightGrey:    '#b6b6b6',
+  lighterGrey:  '#ebebeb',
+  white:        '#ffffff',
+
+  /* Background colours */
+  primaryBg:   '#ffffff',
+  secondaryBg: '#f5f5f5',
+  thirdBg:     '#ebebeb',
+  fourthBg:    '#cccccc',
+  fifthBg:     '#727272',      
+  inverseBg:   '#303f9f',
+
+  /* Text colours */
+  primaryTxt: '#212121',
+  lightTxt:   '#727272',
+  loudTxt:    '#212121',
+  inverseTxt: '#ffffff',
+  headingTxt: '#303f9f',
+
+  /* Link colours */
+  primaryLinkColor:        '#3f51b5',
+  secondaryLinkColorHover: '#727272',
+
+  /* Border colours */
+  primaryBorderColor:   '#b6b6b6',
+  secondaryBorderColor: '#3f51b5',
+}
+
+export { color }
---
+++ src/types/Color.ts
@@ -0,0 +1,37 @@
+type Color = {
+  /* App primary colours */
+  black:        string
+  darkBlue:     string
+  blue:         string
+  lightBlue:    string
+  pink:         string
+  grey:         string
+  lightGrey:    string
+  lighterGrey:  string
+  white:        string
+
+  /* Background colours */
+  primaryBg:   string
+  secondaryBg: string
+  thirdBg:     string
+  fourthBg:    string
+  fifthBg:     string      
+  inverseBg:   string
+
+  /* Text colours */
+  primaryTxt: string
+  lightTxt:   string
+  loudTxt:    string
+  inverseTxt: string
+  headingTxt: string
+
+  /* Link colours */
+  primaryLinkColor:        string
+  secondaryLinkColorHover: string
+
+  /* Border colours */
+  primaryBorderColor:   string
+  secondaryBorderColor: string
+}
+
+export default Color
