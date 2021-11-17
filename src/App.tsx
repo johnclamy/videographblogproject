fdf525bc1ba3546181ce84f5d41157a4a6a732c2
John_Lamy <johnclamy@gmail.com>
2021-11-17 00:11:18 +0000

    implement interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { ThemeProvider } from "styled-components"
 import { color } from './constants'
+import Props from './interfaces/Props'
 
-export default function App({ children }) {
+export default function App({ children }: Props) {
   return (
     <ThemeProvider theme={{ color }}>
       {children}
