57a58be21d3b79ce81cd4e5442d6624e3df48870
John_Lamy <johnclamy@gmail.com>
2021-11-17 01:43:27 +0000

    set global in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { ThemeProvider } from "styled-components"
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
 import { color } from './constants'
 import Props from './types/Props'
 
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', Arial, Helvetica, sans-serif;
+  }
+
+  #root {
+    margin: 0 auto;
+  }
+`
+
 export default function App({ children }: Props) {
   return (
-    <ThemeProvider theme={{ color }}>
-      {children}
-    </ThemeProvider>
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={{ color }}>
+        {children}
+      </ThemeProvider>
+    </>
   )
 }
