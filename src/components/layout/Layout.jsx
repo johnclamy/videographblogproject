c6cf25ac91ee7efda6ee667d7bedee0c05629c50
John_Lamy <johnclamy@gmail.com>
2022-02-17 15:18:47 +0000

    use Layout.jsx as wrapper component for App.jsx

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,9 @@
+const Layout = ({ children }) => (
+  <div className="md:container md:mx-auto">
+    <div className="max-w-3xl mx-auto">
+      {children}
+    </div>
+  </div>
+)
+
+export default Layout
