ad33f03114f06ce1cc10b43db7858dbee0242e55
John_Lamy <johnclamy@gmail.com>
2022-02-17 15:11:59 +0000

    list the todo component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { useState } from 'react';
+import data from './data/initialData'
 import Header from './components/layout/Header';
-import List from './components/layout/List';
+import Todos from './components/todos/Todos'
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState(data);
+
   return (
     <div className="md:container md:mx-auto">
       <div className="max-w-3xl mx-auto">
         <Header />
-        <List />
+        <Todos todos={todos} />
       </div>
     </div>
   );
