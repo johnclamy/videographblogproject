e76577a2345aa9eb09a9cac1654a21b5fc551d21
John_Lamy <johnclamy@gmail.com>
2022-02-17 10:56:29 +0000

    header component

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => (
+  <header className="bg-teal-100 p-4">
+    <h1 className="text-gray-900 font-mono text-4xl tracking-wide text-center capitalize">
+      my todos
+    </h1>
+  </header>
+)
+
+export default Header
