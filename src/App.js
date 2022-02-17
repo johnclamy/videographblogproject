9769ca34e274c2b00aeb0b9d435c654da47cb413
John_Lamy <johnclamy@gmail.com>
2022-02-17 21:21:13 +0000

    insert AddTodo component to App.jsx

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import data from './data/initialData'
 import Layout from './components/layout/Layout';
 import Todos from './components/app/Todos'
 import './App.css';
+import AddTodo from './components/app/AddTodo';
 
 function App() {
   const [todos, setTodos] = useState(data);
+  const addTodo = todo => setTodos([...todos, todo])
 
   return (
     <Layout>
+      <AddTodo onAddTodo={addTodo} />
       <Todos todos={todos} />
     </Layout>
   );
