dfb0c26f0c848b64850ce2b3d39e30c733fc1e7e
John_Lamy <johnclamy@gmail.com>
2022-02-17 10:57:03 +0000

    added Header component to App.jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import Header from './components/layout/Header';
 import './App.css';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className='container mx-auto px-4'>
+      <Header />
+    </div>
+  )
 }
 
 export default App;
