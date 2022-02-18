ea6f2357bf8ee4f455536a76b17b4aad2ca1f91a
John_Lamy <johnclamy@gmail.com>
2022-02-18 22:40:17 +0000

    check to-dos props for data passed down

--- src/components/app/Todos.jsx
+++ src/components/app/Todos.jsx
@@ -6,8 +6,13 @@ const Todos = ({ todos }) => {
       <Todo todo={todo} />
     </ul>
   ));
+  const todosComponent = !todos.length ? (
+    <p className="p-5 rounded-lg text-center text-2xl font-bold bg-teal-300 max-w-screen-sm">Currently you have no to-dos.</p>
+  ) : (
+    todoList
+  );
 
-  return todoList
+  return todosComponent
 }
 
 export default Todos
