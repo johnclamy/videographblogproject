693fcde7078e3fc782febd71ec638f6e98368eb6
John_Lamy <johnclamy@gmail.com>
2022-02-20 02:47:18 +0000

    change props from todos to item

--- src/components/app/Todos.jsx
+++ src/components/app/Todos.jsx
@@ -1,12 +1,14 @@
 import Todo from "./Todo"
 
-const Todos = ({ todos }) => {
-  const todoList = todos.map((todo) => (
-    <ul>
-      <Todo todo={todo} />
-    </ul>
-  ));
-  const todosComponent = !todos.length ? (
+const Todos = ({ items }) => {
+  const todoList = items.map((item) => {
+    return (
+      <ul>
+        <Todo key={item.id} todo={item.todo} />
+      </ul>
+    );
+  });
+  const todosComponent = !items.length ? (
     <p className="p-5 rounded-lg text-center text-2xl font-bold bg-teal-300 max-w-screen-sm">Currently you have no to-dos.</p>
   ) : (
     todoList
