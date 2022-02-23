6d139a3c68e3be2e478f2faa583a71d39b950cb3
John_Lamy <johnclamy@gmail.com>
2022-02-23 00:44:14 +0000

    add buttons to Todo.jsx

--- src/components/app/Todo.jsx
+++ src/components/app/Todo.jsx
@@ -1,6 +1,13 @@
+import EditButton from "../layout/EditButton";
+import DeleteButton from "../layout/DeleteButton";
+
 const Todo = ({ todo }) => (
-  <li className="list-none font-sans text-lg font-medium text-left text-white bg-cyan-400 p-4 my-2">
-    {todo}
+  <li className=" flex flex-row justify-between list-none font-sans text-lg font-medium text-left text-white bg-cyan-400 p-4 my-2">
+    <p>{todo}</p>
+    <div className="flex space-x-4">
+      <EditButton />
+      <DeleteButton />
+    </div>
   </li>
 );
 
