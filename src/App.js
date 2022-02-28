969bbf61389babe22eef780c204e1a716d1ff8fb
John_Lamy <johnclamy@gmail.com>
2022-02-28 23:43:57 +0000

    render todos from local data store

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { useState, useEffect } from 'react'
-import { db, collection, getDocs, addDoc } from './firebase'
+// import { db, collection, getDocs, addDoc } from './firebase'
 import Layout from "./components/layout/Layout";
 import Todos from './components/app/Todos'
 import AddTodo from './components/app/AddTodo';
 import "./App.css";
 
+const tempTodos = [
+  { id: '1', todo: 'feed the dog' },
+  { id: '2', todo: 'cook breakfast' },
+  { id: '3', todo: 'learn a new language' },
+  { id: '4', todo: 'walk the dog'}
+
+]
+
 function App() {
-  const colRef = collection(db, "todos");
-  const [todos, setTodos] = useState([]);
+  // const colRef = collection(db, "todos");
+  const [todos, setTodos] = useState(tempTodos);
 
-  const addTodo = async (todo) => await addDoc(colRef, todo)
-
+   // const addTodo = async (todo) => await addDoc(colRef, todo)
+  /*
   useEffect(() => {
     const getTodos = async () => {
       const dbTodos = await getDocs(colRef)
@@ -18,13 +26,13 @@ function App() {
     }
 
     getTodos()
-  }, []);
+  }, []);*/
 
   console.log(todos)
 
   return (
     <Layout>
-      <AddTodo onAddTodo={addTodo} />
+      <AddTodo /* onAddTodo={addTodo} */ />
       <Todos items={todos} />
     </Layout>
   );
