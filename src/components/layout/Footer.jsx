207c9325400bcc63d84ff2a6d34a8362b560ae75
John_Lamy <johnclamy@gmail.com>
2022-03-01 15:15:53 +0000

    add TodoStats to Footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,9 +1,14 @@
+import TodoStats from "../app/TodoStats";
+
 const Footer = () => {
   return (
-    <footer>
-      footer here
+    <footer className="bg-blue-900 text-white text-center border-t-4 border-teal-100 fixed inset-x-0 bottom-0 p-4 md:container md:mx-auto">
+      <div className="max-w-3xl mx-auto flex justify-evenly">
+        <TodoStats />
+        <button className="bg-red-500 rounded px-2 capitalize">delete all</button>
+      </div>
     </footer>
-  )
+  );
 }
 
 export default Footer
