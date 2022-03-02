4a82a2c1317d13a28190219f573ded93aa54f445
John_Lamy <johnclamy@gmail.com>
2022-03-02 16:25:23 +0000

    add todo object to todos

--- src/App.js
+++ src/App.js
@@ -6,19 +6,18 @@ import AddTodo from './components/app/AddTodo';
 import Footer from './components/layout/Footer'
 
 const tempTodos = [
-  { id: '1', todo: 'feed the dog' },
-  { id: '2', todo: 'cook breakfast' },
-  { id: '3', todo: 'learn a new language' },
-  { id: '4', todo: 'walk the dog'}
-
-]
+  { id: "5326931883580409", todo: "feed the dog" },
+  { id: "7296572133983615", todo: "cook breakfast" },
+  { id: "4821274738313757", todo: "learn a new language" },
+  { id: "5113042383785769", todo: "walk the dog" },
+];
 
 function App() {
   // const colRef = collection(db, "todos");
   const [todos, setTodos] = useState(tempTodos);
 
-   // const addTodo = async (todo) => await addDoc(colRef, todo)
-  /*
+   /* const addTodo = async (todo) => await addDoc(colRef, todo)
+
   useEffect(() => {
     const getTodos = async () => {
       const dbTodos = await getDocs(colRef)
@@ -28,11 +27,16 @@ function App() {
     getTodos()
   }, []);*/
 
+  const addTodo = (todo) => {
+    todo.id = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+    setTodos([...todos, todo])
+  }
+
   console.log(todos)
 
   return (
     <Layout>
-      <AddTodo /* onAddTodo={addTodo} */ />
+      <AddTodo onAddTodo={addTodo} />
       <Todos items={todos} />
       <Footer />
     </Layout>
