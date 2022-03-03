6e13b354526170fe478e7847450486edab7b29de
John_Lamy <johnclamy@gmail.com>
2022-03-03 14:51:44 +0000

    add isCompleted property to tempTodos object

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import AddTodo from './components/app/AddTodo';
 import Footer from './components/layout/Footer'
 
 const tempTodos = [
-  { id: "5326931883580409", todo: "feed the dog" },
-  { id: "7296572133983615", todo: "cook breakfast" },
-  { id: "4821274738313757", todo: "learn a new language" },
-  { id: "5113042383785769", todo: "walk the dog" },
+  { id: "5326931883580409", todo: "feed the dog", isCompleted: false },
+  { id: "7296572133983615", todo: "cook breakfast", isCompleted: false },
+  { id: "4821274738313757", todo: "learn a new language", isCompleted: false },
+  { id: "5113042383785769", todo: "walk the dog", isCompleted: false },
 ];
 
 function App() {
