35483fc981fcbfca09579acefa66a7ba76428008
John_Lamy <johnclamy@gmail.com>
2022-03-03 16:06:53 +0000

    useState to track line-through each todo item

--- src/components/app/Todo.jsx
+++ src/components/app/Todo.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react'
 import EditButton from "../layout/EditButton";
 import DeleteButton from "../layout/DeleteButton";
 
 const Todo = ({ item, onDeleteTodo }) => {
+  const [lineThrough, setLineThrough] = useState(false)
+  const itemTodo = !lineThrough
+    ? <p>{item.todo}</p>
+    : <p className='line-through'>{item.todo}</p>;
+
   return (
     <li className=" flex flex-row justify-between list-none font-sans text-lg font-medium text-left text-white bg-cyan-400 p-4 my-2">
-      <p>{item.todo}</p>
+      <div
+        className="cursor-pointer"
+        onClick={() => setLineThrough(!lineThrough)}
+      >
+        {itemTodo}
+      </div>
       <div className="flex space-x-4">
         <EditButton />
         <DeleteButton id={item.id} onDeleteTodo={onDeleteTodo} />
@@ -12,6 +23,5 @@ const Todo = ({ item, onDeleteTodo }) => {
     </li>
   );
 }
-
 
 export default Todo
