9eaf1ad19fc93a015f68717085e9816b7e798823
John_Lamy <johnclamy@gmail.com>
2022-03-03 13:36:59 +0000

    apply deleteTodos function in Footer.jsx

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,14 +1,16 @@
 import TodoStats from "../app/TodoStats";
 
-const Footer = ({ items }) => {
+const Footer = ({ items, onDeleteTodos }) => {
   return (
     <footer className="bg-blue-900 text-white text-center border-t-4 border-teal-100 fixed inset-x-0 bottom-0 p-4 md:container md:mx-auto">
       <div className="max-w-3xl mx-auto flex justify-evenly">
         <TodoStats items={items} />
-        <button className="bg-red-500 rounded px-2 capitalize">delete all</button>
+        <button className="bg-red-500 rounded px-2 capitalize" onClick={onDeleteTodos}>
+          delete all
+        </button>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer
