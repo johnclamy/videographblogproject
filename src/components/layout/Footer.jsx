bafca745294a5f4270eb0ccb6332a16599dd3830
John_Lamy <johnclamy@gmail.com>
2022-03-04 00:06:54 +0000

    apply prettier  code format

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -5,7 +5,10 @@ const Footer = ({ items, onDeleteTodos }) => {
     <footer className="bg-blue-900 text-white text-center border-t-4 border-teal-100 fixed inset-x-0 bottom-0 p-4 md:container md:mx-auto">
       <div className="max-w-3xl mx-auto flex justify-evenly">
         <TodoStats items={items} />
-        <button className="bg-red-500 rounded px-2 capitalize" onClick={onDeleteTodos}>
+        <button
+          className="bg-red-500 rounded px-2 capitalize"
+          onClick={onDeleteTodos}
+        >
           delete all
         </button>
       </div>
