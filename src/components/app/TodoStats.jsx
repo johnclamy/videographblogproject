5e49fd6a4e8220ee6138003cd77e5015b3579e8c
John_Lamy <johnclamy@gmail.com>
2022-03-04 00:03:45 +0000

    update stats component

--- src/components/app/TodoStats.jsx
+++ src/components/app/TodoStats.jsx
@@ -2,14 +2,19 @@ const TodoStats = ({ items }) => {
   return (
     <>
       <p className="flex align-middle font-bold capitalize">
-        total: <span className="bg-amber-500 rounded-full px-2 ml-2">{items.length}</span>
+        total:{" "}
+        <span className="bg-amber-500 rounded-full px-2 ml-2">
+          {items.length}
+        </span>
       </p>
       <p className="flex align-middle font-bold capitalize">
         completed:{" "}
-        <span className="bg-white rounded-full px-2 ml-2 text-blue-900">2</span>
+        <span className="bg-white rounded-full px-2 ml-2 text-blue-900">
+          {(items.filter(item => item.isCompleted)).length}
+        </span>
       </p>
     </>
   );
-}
+};
 
 export default TodoStats
