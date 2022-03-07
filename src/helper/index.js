d778fedf0015127b679e58ffd82f448914cbb7d3
John_Lamy <johnclamy@gmail.com>
2022-03-07 12:16:06 +0000

    set helper func to generate unique id

--- src/helper/index.js
+++ src/helper/index.js
@@ -1,3 +1,4 @@
 const filterListById = (list, id) => list.filter((item) => item.id !== id);
+const createUniqueId = () => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
 
-export { filterListById }
+export { createUniqueId, filterListById }
