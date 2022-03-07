682609c4e36b7bcac44525ee8f42b9464c5b6446
John_Lamy <johnclamy@gmail.com>
2022-03-07 12:04:25 +0000

    store temp todos in data folder

---
+++ src/data/index.js
@@ -0,0 +1,8 @@
+const tempTodos = [
+  { id: "5326931883580409", todo: "feed the dog", isCompleted: false },
+  { id: "7296572133983615", todo: "cook breakfast", isCompleted: false },
+  { id: "4821274738313757", todo: "learn a new language", isCompleted: false },
+  { id: "5113042383785769", todo: "walk the dog", isCompleted: false },
+];
+
+export default tempTodos
