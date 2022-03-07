8ed352b489205a5132ce26c1044446be2b4b1e90
John_Lamy <johnclamy@gmail.com>
2022-03-07 14:34:30 +0000

    render edited todo content to input field

--- src/components/app/AddTodo.jsx
+++ src/components/app/AddTodo.jsx
@@ -1,20 +1,22 @@
 import { useState } from "react"
 
-const AddTodo = ({ onAddTodo }) => {
+const AddTodo = ({ onAddTodo, editedTodo }) => {
   const [todo, setTodo] = useState("");
 
   const handleClick = (e) => {
     e.preventDefault();
-    onAddTodo({ todo })
-    setTodo('')
+    onAddTodo({ todo });
+    setTodo("");
   };
+
+  console.log(editedTodo)
 
   return (
     <form className=" flex justify-center my-3 p-6 bg-amber-400">
       <input
         className="border py-2 px-3 text-gray-900"
         type="input"
-        value={todo}
+        value={!editedTodo ? todo : editedTodo.todo}
         onChange={(e) => setTodo(e.target.value)}
         placeholder="what to do next..."
       />
