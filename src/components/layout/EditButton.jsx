ff4663532650fccda5d8ad05f54a88affd5fcd0a
John_Lamy <johnclamy@gmail.com>
2022-03-07 13:56:58 +0000

    handle click event on todo item

--- src/components/layout/EditButton.jsx
+++ src/components/layout/EditButton.jsx
@@ -1,6 +1,6 @@
-const EditButton = ({ onEditTodo }) => {
+const EditButton = ({ item, onEditTodo }) => {
   return (
-    <button onClick={onEditTodo}>
+    <button onClick={() => onEditTodo(item)}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         className="h-6 w-6"
