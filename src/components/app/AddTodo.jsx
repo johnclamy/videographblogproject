a270399bc04c1c77b7a942762c2c1a6800b61d96
John_Lamy <johnclamy@gmail.com>
2022-03-09 00:29:56 +0000

    render toggle btns for edit or add todo

--- src/components/app/AddTodo.jsx
+++ src/components/app/AddTodo.jsx
@@ -1,26 +1,42 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import AddButton from "../layout/AddButton";
+import UpdateButton from "../layout/UpdateButton";
 
 const AddTodo = ({ onAddTodo, editedTodo }) => {
-  const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState('');
 
   const handleAddCurrentTodo = () => {
     onAddTodo({ todo });
     setTodo("");
   };
 
-  console.log(editedTodo)
+  const toggleButton = !editedTodo ? (
+    <AddButton onAddCurrentTodo={handleAddCurrentTodo} />
+  ) : (
+    <UpdateButton
+      onAddCurrentTodo={handleAddCurrentTodo}
+    />
+  );
+
+  useEffect(() => {
+    if (editedTodo) {
+      setTodo(editedTodo.todo)
+    }
+  }, [])
+
+  console.log('singletodo:', todo)
 
   return (
     <form className=" flex justify-center my-3 p-6 bg-amber-400">
       <input
         className="border py-2 px-3 text-gray-900"
         type="input"
-        value={!editedTodo ? todo : editedTodo.todo}
+        value={todo}
         onChange={(e) => setTodo(e.target.value)}
-        placeholder="what to do next..."
+        placeholder={editedTodo ? editedTodo.todo : "what to do next..."}
       />
-      <AddButton onAddCurrentTodo={handleAddCurrentTodo} />
+
+      {toggleButton}
     </form>
   );
 };
