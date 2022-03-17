59490269fa449ebc42ec35204ea9f17248ccc3f8
John_Lamy <johnclamy@gmail.com>
2022-03-17 16:03:40 +0000

    fetch data from Firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
-import { db, collection, getDocs, addDoc } from './firebase'
-import { createUniqueId, removeItemFrom } from './helper';
+import { db, collection, onSnapshot } from './firebase'
+import { removeItemFrom } from './helper';
 import Layout from "./components/layout/Layout";
 import Todos from './components/app/Todos'
 import AddTodo from './components/app/AddTodo';
@@ -9,67 +9,57 @@ import Footer from './components/layout/Footer'
 function App() {
   const colRef = collection(db, "todos");
   const [todos, setTodos] = useState([]);
-  const [editedTodo, setEditedTodo] = useState(null)
-
-   // const addTodo = async (todo) => await addDoc(colRef, todo)
+  const [editedTodo, setEditedTodo] = useState(null);
 
   useEffect(() => {
-    const getTodos = async () => {
-      const dbTodos = await getDocs(colRef)
-      setTodos(dbTodos.docs.map(doc => ({ ...doc.data(), id: doc.id })))
-    }
+    onSnapshot(colRef, (snap) => {
+      const todos = [];
+      snap.docs.forEach((doc) => {
+        todos.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todos);
+      console.log(todos);
+    });
+  }, [])
 
-    getTodos()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const addTodo = todo => {
-    if (!todo.id) {
-      todo.id = createUniqueId().toString()
-      todo.isCompleted = false
-    }
-    setTodos([...todos, todo])
-  }
-
-  const editTodo = todo => {
-    const id = todo.id
-    const list = removeItemFrom(todos, id)
+  const editTodo = (todo) => {
+    const id = todo.id;
+    const list = removeItemFrom(todos, id);
     setEditedTodo(todos.find((item) => item.id === id));
-    setTodos(list)
-  }
+    setTodos(list);
+  };
 
-  const deleteTodo = id => setTodos(removeItemFrom(todos, id));
+  const deleteTodo = (id) => setTodos(removeItemFrom(todos, id));
 
   const deleteTodos = () => {
-    let emptyList = todos
-    emptyList = []
-    setTodos(emptyList)
-  }
+    let emptyList = todos;
+    emptyList = [];
+    setTodos(emptyList);
+  };
 
-  const toggleComplete = id => {
-    const toggledTodos = todos.filter(todo => {
+  const toggleComplete = (id) => {
+    const toggledTodos = todos.filter((todo) => {
       if (todo.id === id) {
-        todo.isCompleted = !todo.isCompleted
-        return todo
+        todo.isCompleted = !todo.isCompleted;
+        return todo;
       }
-      return todo
-    })
-    setTodos(toggledTodos)
-  }
+      return todo;
+    });
+    setTodos(toggledTodos);
+  };
 
   const emptyEditedTodo = () => {
     if (editedTodo) {
-      let todo = editedTodo
-      todo = null
-      setEditedTodo(todo)
+      let todo = editedTodo;
+      todo = null;
+      setEditedTodo(todo);
     }
-  }
+  };
 
   return (
     <Layout>
       <AddTodo
         editedTodo={editedTodo}
-        onAddTodo={addTodo}
         onEmptyEditedTodo={emptyEditedTodo}
       />
       <Todos
