508e4e480e15581b07034cd8e5ecf1603b16dcf4
John_Lamy <johnclamy@gmail.com>
2022-03-21 00:26:26 +0000

    Add Signin component to Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,12 @@
+import Signin from './Signin'
+
 const Header = () => (
-  <header className="bg-teal-100 p-4">
-    <h1 className="text-gray-900 font-mono text-4xl tracking-wide text-center capitalize">
+  <header className="bg-teal-100 p-4 flex">
+    <h1 className="text-gray-900 font-mono text-4xl tracking-wide text-center capitalize flex-1">
       my todos
     </h1>
+    <Signin />
   </header>
-)
+);
 
 export default Header
