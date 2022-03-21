f0941b22bd109d3dcd8b0df02c00a9755b28a7bd
John_Lamy <johnclamy@gmail.com>
2022-03-21 17:12:14 +0000

    replace SignOut.jsx with SignOutIcon.jsx

--- src/components/layout/SignOutIcon.jsx
+++ src/components/layout/SignOutIcon.jsx
@@ -1,4 +1,4 @@
-const SignOut = () => {
+const SignOutIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -17,4 +17,4 @@ const SignOut = () => {
   );
 }
 
-export default SignOut
+export default SignOutIcon
