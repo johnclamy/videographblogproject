e611a3d562abd2f8ad8922923d9559e59fb89321
John_Lamy <johnclamy@gmail.com>
2022-03-21 10:16:09 +0000

    change component name in SigninIcon.jsx

--- src/components/layout/Signin.jsx
+++ src/components/layout/Signin.jsx
@@ -1,4 +1,4 @@
-const Signin = () => {
+const SigninIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -17,4 +17,4 @@ const Signin = () => {
   );
 }
 
-export default Signin
+export default SigninIcon
