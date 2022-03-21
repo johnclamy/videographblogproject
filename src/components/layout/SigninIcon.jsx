30d13a35bfe81ca70d3f8a8dfba24a1ff9f35955
John_Lamy <johnclamy@gmail.com>
2022-03-21 17:56:00 +0000

    set cursor to pointer

--- src/components/layout/SigninIcon.jsx
+++ src/components/layout/SigninIcon.jsx
@@ -2,7 +2,7 @@ const SigninIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      className="h-6 w-6 self-center"
+      className="h-6 w-6 self-center cursor-pointer"
       fill="none"
       viewBox="0 0 24 24"
       stroke="currentColor"
