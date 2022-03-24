c690b749e7c1ba9a4c622f3ed21786df25ce0c11
John_Lamy <johnclamy@gmail.com>
2022-03-24 16:04:08 +0000

    disable delete all btn when not signed in

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,13 +1,24 @@
 import TodoStats from "../app/TodoStats";
+import Tooltip from './Tooltip'
 
-const Footer = ({ items, onDeleteTodos }) => {
+const Footer = ({
+  items,
+  signedIn,
+  onDeleteTodos,
+  showButtonTooltip,
+  onToggleShowButtonTooltip,
+}) => {
   return (
     <footer className="bg-blue-900 text-white text-center border-t-4 border-teal-100 fixed inset-x-0 bottom-0 p-4 md:container md:mx-auto">
       <div className="max-w-3xl mx-auto flex justify-evenly">
         <TodoStats items={items} />
+        {showButtonTooltip && <Tooltip>Please sign in to delete todos</Tooltip>}
         <button
+          disabled={!signedIn}
           className="bg-red-500 rounded px-2 capitalize"
           onClick={onDeleteTodos}
+          onMouseEnter={() => onToggleShowButtonTooltip()}
+          onMouseLeave={() => onToggleShowButtonTooltip()}
         >
           delete all
         </button>
