3a702043e2cf3e005d643dfebd88b271efadebdf
John_Lamy <johnclamy@gmail.com>
2022-03-24 12:50:34 +0000

    add shadow to tooltip

--- src/components/layout/Tooltip.jsx
+++ src/components/layout/Tooltip.jsx
@@ -1,5 +1,5 @@
 const Tooltip = ({ children }) => (
-  <div className="text-sm bg-gray-700 text-gray-100 px-1 absolute">
+  <div className="text-sm bg-gray-600 text-gray-100 px-1 absolute rounded bg-opacity-50 shadow-xl">
     {children}
   </div>
 )
