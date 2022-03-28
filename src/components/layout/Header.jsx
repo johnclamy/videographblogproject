c1bcc65d2fb8e034ff6b25ef4aeaa693964fad5d
John_Lamy <johnclamy@gmail.com>
2022-03-28 14:03:43 +0100

    link icon btn to sign in page

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,11 +1,14 @@
+import { Link } from 'react-router-dom';
 import SigninIcon from './SigninIcon';
 
 const Header = () => (
   <header className="bg-teal-100 p-4 flex">
     <h1 className="text-gray-900 font-mono text-4xl tracking-wide text-center capitalize flex-1">
-      my todos
+      <Link to="/">my todos</Link>
     </h1>
-    <SigninIcon />
+    <Link to="/signin">
+      <SigninIcon />
+    </Link>
   </header>
 );
 
