2707b71193ccb177b35cc114cc47bf33e7615b4b
John_Lamy <johnclamy@gmail.com>
2022-03-28 14:14:52 +0100

    align header bar items

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import SigninIcon from './SigninIcon';
 
 const Header = () => (
-  <header className="bg-teal-100 p-4 flex">
+  <header className="bg-teal-100 p-4 flex items-center">
     <h1 className="text-gray-900 font-mono text-4xl tracking-wide text-center capitalize flex-1">
       <Link to="/">my todos</Link>
     </h1>
