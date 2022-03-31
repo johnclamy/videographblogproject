55b7c49b41dcf3d1e4d08707a679efcbb119852d
John_Lamy <johnclamy@gmail.com>
2022-03-31 17:05:40 +0100

    toggle data from the sign in form

--- src/components/layout/SigninForm.jsx
+++ src/components/layout/SigninForm.jsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 
-const SigninForm = () => {
+const SigninForm = ({ onToggleSignin }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleSubmit = e => {
+    e.preventDefault()
+    onToggleSignin()
+    console.log(username, password)
+    setUsername('')
+    setPassword('')
+  }
+
   return (
-    <form className="flex flex-col items-center my-3 p-6 bg-amber-400">
+    <form onSubmit={handleSubmit} className="flex flex-col items-center my-3 p-6 bg-amber-400">
       <h1 className="text-black pt-2 pb-4 text-xl font-bold text-center">
         Sign In
       </h1>
@@ -19,6 +27,7 @@ const SigninForm = () => {
             className="border py-2 px-3 text-gray-900"
             type="text"
             value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
         <div className="flex justify-between">
@@ -30,9 +39,10 @@ const SigninForm = () => {
             className="border py-2 px-3 text-gray-900"
             type="password"
             value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button className="mt-6 py-2 px-4 bg-slate-400 hover:bg-slate-600 text-white uppercase">
+        <button type='submit' className="mt-6 py-2 px-4 bg-slate-400 hover:bg-slate-600 text-white uppercase">
           sign in
         </button>
       </div>
