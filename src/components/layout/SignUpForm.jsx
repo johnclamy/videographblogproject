664aaa37e9a987ff976345d7b879db24d4a1dee1
John_Lamy <johnclamy@gmail.com>
2022-03-31 14:27:54 +0100

    add a max width to form

--- src/components/layout/SignUpForm.jsx
+++ src/components/layout/SignUpForm.jsx
@@ -5,43 +5,48 @@ const SignUpForm = () => {
   const [passwordOne, setPasswordOne] = useState('')
   const [passwordTwo, setPasswordTwo] = useState("");
   return (
-    <form className="bg-amber-400 p-4 mt-3 text-center">
-      <h1 className="text-black pt-2 pb-4 text-xl font-bold">Sign Up</h1>
-      <div className=" flex flex-col p-8 bg-slate-50">
-        <div className="flex justify-between">
-          <label className="text-lg text-gray-800" htmlFor="username">
+    <form className="flex flex-col items-center my-3 p-6 bg-amber-400">
+      <h1 className="text-black pt-2 pb-4 text-xl font-bold text-center">
+        Sign Up
+      </h1>
+      <div className=" flex flex-col p-8 bg-slate-50 max-w-md">
+        <div className="flex justify-between align-center">
+          <label
+            className="text-md text-gray-800"
+            htmlFor="username"
+          >
             username
           </label>
           <input
-            id='username'
-            className="bg-gray-200 text-sm px-2 py-1 border border-gray-500"
+            id="username"
+            className="border py-2 px-3 text-gray-900"
             type="text"
             value={username}
           />
         </div>
         <div className="flex justify-between">
-          <label className="text-lg text-gray-800" htmlFor="passwordOne">
+          <label className="text-md text-gray-800" htmlFor="passwordOne">
             password
           </label>
           <input
-            id='passwordOne'
-            className="bg-gray-200 text-sm px-2 py-1 border border-gray-500"
+            id="passwordOne"
+            className="border py-2 px-3 text-gray-900"
             type="password"
             value={passwordOne}
           />
         </div>
         <div className="flex justify-between">
-          <label className="text-lg text-gray-800" htmlFor="passwordTwo">
+          <label className="text-md text-gray-800" htmlFor="passwordTwo">
             confirm password
           </label>
           <input
-            id='passwordTwo'
-            className="bg-gray-200 text-sm px-2 py-1 border border-gray-500"
+            id="passwordTwo"
+            className="border py-2 px-3 text-gray-900"
             type="password"
             value={passwordTwo}
           />
         </div>
-        <button>sign up</button>
+        <button className='mt-6 py-2 px-4 bg-slate-400 hover:bg-slate-600 text-white uppercase'>sign up</button>
       </div>
     </form>
   );
