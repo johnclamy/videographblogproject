bda9a62592b0e8343cc063d823d448b15a96cbed
John_Lamy <johnclamy@gmail.com>
2022-04-01 01:22:52 +0100

    toggle auth btns with boolean signedIn condition

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,15 +1,25 @@
 import { Link } from 'react-router-dom';
 import SigninIcon from './SigninIcon';
+import SignOutIcon from './SignOutIcon'
 
-const Header = () => (
-  <header className="bg-teal-100 p-4 flex items-center">
-    <h1 className="text-gray-900 font-mono text-4xl tracking-wide text-center capitalize flex-1">
-      <Link to="/">my todos</Link>
-    </h1>
+const Header = ({ signedIn }) => {
+  const authIcon = signedIn ? (
+    <Link to="/signout">
+      <SignOutIcon />
+    </Link>
+  ) : (
     <Link to="/signin">
       <SigninIcon />
     </Link>
-  </header>
-);
+  );
+  return (
+    <header className="bg-teal-100 p-4 flex items-center">
+      <h1 className="text-gray-900 font-mono text-4xl tracking-wide text-center capitalize flex-1">
+        <Link to="/">my todos</Link>
+      </h1>
+      {authIcon}
+    </header>
+  );
+}
 
 export default Header
