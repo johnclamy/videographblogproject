b3b5bcdc9608ef279fff8a6ff46232cfe08e8242
John_Lamy <johnclamy@gmail.com>
2022-04-03 23:50:19 +0100

    naviogate to home page if logged in

--- src/components/layout/SigninForm.jsx
+++ src/components/layout/SigninForm.jsx
@@ -1,19 +1,25 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const SigninForm = ({ onToggleSignin }) => {
+const SigninForm = ({ signedIn, onToggleSignin }) => {
+  const navigate = useNavigate()
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = e => {
-    e.preventDefault()
-    onToggleSignin()
-    console.log(username, password)
-    setUsername('')
-    setPassword('')
-  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onToggleSignin();
+    console.log(username, password);
+    setUsername("");
+    setPassword("");
+    navigate('/')
+  };
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col items-center my-3 p-6 bg-amber-400">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center my-3 p-6 bg-amber-400"
+    >
       <h1 className="text-black pt-2 pb-4 text-xl font-bold text-center">
         Sign In
       </h1>
@@ -42,7 +48,10 @@ const SigninForm = ({ onToggleSignin }) => {
             onChange={(e) => setPassword(e.target.value)}
           />
         </div>
-        <button type='submit' className="mt-6 py-2 px-4 bg-slate-400 hover:bg-slate-600 text-white uppercase">
+        <button
+          type="submit"
+          className="mt-6 py-2 px-4 bg-slate-400 hover:bg-slate-600 text-white uppercase"
+        >
           sign in
         </button>
       </div>
