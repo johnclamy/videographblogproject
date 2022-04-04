72d3a21d0649c405c70097b360244975d1e4cb79
John_Lamy <johnclamy@gmail.com>
2022-04-04 14:45:35 +0100

    correct typo in onToggleSignin

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,9 +1,9 @@
 import Header from "./Header"
 
-const Layout = ({ signedIn, onToggleSignin, children }) => (
+const Layout = ({ signedIn, onToggleSignIn, children }) => (
   <div className="md:container md:mx-auto">
     <div className="max-w-3xl mx-auto">
-      <Header signedIn={signedIn} onToggleSignin={onToggleSignin} />
+      <Header signedIn={signedIn} onToggleSignIn={onToggleSignIn} />
       {children}
     </div>
   </div>
