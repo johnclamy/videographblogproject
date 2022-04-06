87458a18b6f39ab2dfd383c8d0112b4defed69be
John_Lamy <johnclamy@gmail.com>
2022-04-06 12:23:52 +0100

    implement auth functions

--- src/components/auth/index.js
+++ src/components/auth/index.js
@@ -1,2 +1,44 @@
-// import { auth } from '../firebase'
+import {
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "../../firebase";
 
+const signUserUp = (email, password) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((err) => {
+      const errorCode = err.code;
+      const errorMsg = err.message;
+      console.error(errorCode, errorMsg);
+    });
+};
+
+const signUserIn = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log(user);
+    })
+    .catch((err) => {
+      const errorCode = err.code;
+      const errorMsg = err.message;
+      console.error(errorCode, errorMsg);
+    });
+};
+
+const signUserOut = () => {
+  signOut(auth)
+    .then(() => {
+      console.log("User was signed out successfully");
+    })
+    .catch((err) => {
+      console.error("user failed to sign in", err.message);
+    });
+};
+
+export { signUserUp, signUserIn, signUserOut };
