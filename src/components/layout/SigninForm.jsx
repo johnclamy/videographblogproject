0c49ab0ae3d1ddf9d5d42591d1133d8dc9daef14
John_Lamy <johnclamy@gmail.com>
2022-04-08 03:34:14 +0100

    move error msg into form tag

--- src/components/layout/SigninForm.jsx
+++ src/components/layout/SigninForm.jsx
@@ -23,49 +23,47 @@ const SigninForm = ({ onToggleSignin }) => {
   };
 
   return (
-    <>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col items-center my-3 p-6 bg-amber-400"
-      >
-        <h1 className="text-black pt-2 pb-4 text-xl font-bold text-center">
-          Sign In
-        </h1>
-        <div className=" flex flex-col p-8 bg-slate-50 max-w-md">
-          <div className="flex justify-between align-center">
-            <label className="text-md text-gray-800" htmlFor="email">
-              email
-            </label>
-            <input
-              id="email"
-              className="border py-2 px-3 text-gray-900"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="flex justify-between">
-            <label className="text-md text-gray-800" htmlFor="password">
-              password
-            </label>
-            <input
-              id="password"
-              className="border py-2 px-3 text-gray-900"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button
-            type="submit"
-            className="mt-6 py-2 px-4 bg-slate-400 hover:bg-slate-600 text-white uppercase"
-          >
-            sign in
-          </button>
-          {error && <p className="bg-white py-2 px-5 text-center text-red-600 font-bold">{error}</p>}
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center my-3 p-6 bg-amber-400"
+    >
+      <h1 className="text-black pt-2 pb-4 text-xl font-bold text-center">
+        Sign In
+      </h1>
+      <div className=" flex flex-col p-8 bg-slate-50 max-w-md">
+        <div className="flex justify-between align-center">
+          <label className="text-md text-gray-800" htmlFor="email">
+            email
+          </label>
+          <input
+            id="email"
+            className="border py-2 px-3 text-gray-900"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
-      </form>
-    </>
+        <div className="flex justify-between">
+          <label className="text-md text-gray-800" htmlFor="password">
+            password
+          </label>
+          <input
+            id="password"
+            className="border py-2 px-3 text-gray-900"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          type="submit"
+          className="mt-6 py-2 px-4 bg-slate-400 hover:bg-slate-600 text-white uppercase"
+        >
+          sign in
+        </button>
+        {error && <p className="bg-white py-2 px-5 text-center text-red-600 font-bold">{error}</p>}
+      </div>
+    </form>
   );
 };
 
