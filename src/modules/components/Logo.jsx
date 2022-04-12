c5a1656fd155bd584eac7fdce506e559e9169e0c
John_Lamy <johnclamy@gmail.com>
2022-04-12 12:41:36 +0100

    change logo text colour to app orange

--- src/modules/components/Logo.jsx
+++ src/modules/components/Logo.jsx
@@ -3,7 +3,7 @@ const Logo = () => {
     <>
       <a href="/" className="flex items-center py-4 px2">
         <img src="/logo192.png" alt="Logo" className="h-8 w-8 mr-2" />
-        <span className="font-semibold text-gray-500 text-lg uppercase">
+        <span className="font-semibold text-orange-600 text-lg uppercase">
           todo app
         </span>
       </a>
