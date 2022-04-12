6f7fe46e0048ba7479ca213959fcb880842f60cb
John_Lamy <johnclamy@gmail.com>
2022-04-12 15:04:34 +0100

    convert svg component to button

--- src/modules/components/MenuButton.jsx
+++ src/modules/components/MenuButton.jsx
@@ -1,16 +1,20 @@
 const MenuButton = () => (
-  <svg
-    class="w-6 h-6 text-gray-500"
-    x-show="!showMenu"
-    fill="none"
-    stroke-linecap="round"
-    stroke-linejoin="round"
-    stroke-width="2"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-  >
-    <path d="M4 6h16M4 12h16M4 18h16"></path>
-  </svg>
+  <div className="md:hidden flex items-center">
+    <button className="outline-none mobile-nemu-button">
+      <svg
+        class="w-6 h-6 text-gray-500"
+        x-show="!showMenu"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path d="M4 6h16M4 12h16M4 18h16"></path>
+      </svg>
+    </button>
+  </div>
 );
 
 export default MenuButton
