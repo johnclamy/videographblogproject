bd1a1dfdb38b44e982053ca60a9e66edbcc9f16d
John_Lamy <johnclamy@gmail.com>
2022-04-12 14:57:44 +0100

    make auth links visible on mobile navbar

--- src/modules/layout/AuthLinks.jsx
+++ src/modules/layout/AuthLinks.jsx
@@ -1,6 +1,6 @@
 const AuthLinks = () => {
   return (
-    <div className="hidden md:flex items-center space-x-3">
+    <div className="flex items-center space-x-3">
       <a
         href="/"
         className="py-2 px-2 font-medium text-blue-900 rounded hover:bg-orange-600 hover:text-white transition duration-300"
