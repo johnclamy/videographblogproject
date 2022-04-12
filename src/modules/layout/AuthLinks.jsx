6e415b4d2323cdadd93461d58912f4000664cac0
John_Lamy <johnclamy@gmail.com>
2022-04-12 13:07:28 +0100

    add btn style to secondary links in AuthLinks.jsx

--- src/modules/layout/AuthLinks.jsx
+++ src/modules/layout/AuthLinks.jsx
@@ -3,13 +3,13 @@ const AuthLinks = () => {
     <div className="hidden md:flex items-center space-x-3">
       <a
         href="/"
-        className="py-4 px-2 text-blue-900 font-semibold hover:text-orange-600 transition duration-300"
+        className="py-2 px-2 font-medium text-blue-900 rounded hover:bg-orange-600 hover:text-white transition duration-300"
       >
         Sign Up
       </a>
       <a
         href="/"
-        className="py-4 px-2 text-blue-900 font-semibold hover:text-orange-600 transition duration-300"
+        className="py-2 px-2 font-medium text-white bg-orange-600 rounded hover:bg-orange-600 transition duration-300"
       >
         Sign In
       </a>
