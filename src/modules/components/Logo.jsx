9ad1bcfb588ed7aecfba30a24c6bd4218b70c9a7
John_Lamy <johnclamy@gmail.com>
2022-04-12 11:50:42 +0100

    logo in Logo.jsx

---
+++ src/modules/components/Logo.jsx
@@ -0,0 +1,14 @@
+const Logo = () => {
+  return (
+    <>
+      <a href="/" className="flex items-center py-4 px2">
+        <img src="/logo192.png" alt="Logo" className="h-8 w-8 mr-2" />
+        <span className="font-semibold text-gray-500 text-lg uppercase">
+          todo app
+        </span>
+      </a>
+    </>
+  )
+}
+
+export default Logo
