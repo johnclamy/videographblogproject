4f08f199e145c26a857612805ad007c47fe67c43
John_Lamy <johnclamy@gmail.com>
2022-04-13 10:05:02 +0100

    setup a menu toggle func for mobile screens

--- src/modules/layout/Navbar.jsx
+++ src/modules/layout/Navbar.jsx
@@ -1,20 +1,28 @@
+import { useState } from 'react'
 import AuthLinks from "./AuthLinks";
 import TodoLinks from './TodoLinks';
+import TodoDropdownLinks from './TodoDropdownLinks';
 import Logo from "../components/Logo";
 
 const Navbar = () => {
+  const [showDropdownMenu, setShowDropdownMenu] = useState(false)
+  const toggleMenu = () => setShowDropdownMenu(!showDropdownMenu)
+
   return (
-    <nav className="bg-white shadow-lg">
-      <div className="max-w-6xl mx-auto px-4">
-        <section className="flex justify-between items-center">
-          <section className="flex space-x-7 items-center">
-            <Logo />
-            <TodoLinks />
+    <>
+      <nav className="bg-white shadow-lg">
+        <div className="max-w-6xl mx-auto px-4">
+          <section className="flex justify-between items-center">
+            <section className="flex space-x-7 items-center">
+              <Logo />
+              <TodoLinks />
+            </section>
+            <AuthLinks toggleMenu={toggleMenu} />
           </section>
-          <AuthLinks />
-        </section>
-      </div>
-    </nav>
+        </div>
+      </nav>
+      {showDropdownMenu && <TodoDropdownLinks />}
+    </>
   );
 };
 
