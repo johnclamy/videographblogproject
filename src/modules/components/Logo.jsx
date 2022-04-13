49b60220b3b364bf6683e43ecff99875a8dfb51c
John_Lamy <johnclamy@gmail.com>
2022-04-13 15:40:51 +0100

    add link to logo

--- src/modules/components/Logo.jsx
+++ src/modules/components/Logo.jsx
@@ -1,7 +1,12 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useNavigate } from 'react-router-dom' 
+
 const Logo = () => {
+  const navigate = useNavigate()
+
   return (
     <>
-      <a href="/" className="flex items-center py-4 px2">
+      <a onClick={() => navigate('/')} className="cursor-pointer flex items-center py-4 px2">
         <img src="/logo192.png" alt="Logo" className="h-8 w-8 mr-2" />
         <span className="font-semibold text-orange-600 text-lg uppercase">
           todo app
