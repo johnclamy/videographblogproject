7afc55f1b4d59d5952e19a14b11cd15714025697
John_Lamy <johnclamy@gmail.com>
2022-04-13 15:47:11 +0100

    add click redirect to auth btns

--- src/modules/layout/AuthLinks.jsx
+++ src/modules/layout/AuthLinks.jsx
@@ -1,18 +1,20 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { useNavigate } from 'react-router-dom'
 import MenuButton from "../components/MenuButton";
 
 const AuthLinks = ({ toggleMenu }) => {
+  const navigate = useNavigate()
+
   return (
     <div className="flex items-center space-x-3">
       <a
-        href="/"
-        className="py-2 px-2 font-medium text-blue-900 rounded hover:bg-orange-600 hover:text-white transition duration-300"
-      >
+        onClick={() => navigate('/signup')}
+        className="cursor-pointer py-2 px-2 font-medium text-blue-900 rounded hover:bg-orange-600 hover:text-white transition duration-300">
         Sign Up
       </a>
       <a
-        href="/"
-        className="py-2 px-2 font-medium text-white bg-orange-600 rounded hover:bg-orange-600 transition duration-300"
-      >
+        onClick={() => navigate('/signin')}
+        className="cursor-pointer py-2 px-2 font-medium text-white bg-orange-600 rounded hover:bg-orange-600 transition duration-300">
         Sign In
       </a>
       <MenuButton onToggleMenu={toggleMenu} />
