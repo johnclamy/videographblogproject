6753df928e74bb8c6d30b702aa8e7e2314d78c91
John_Lamy <johnclamy@gmail.com>
2022-04-15 00:50:53 +0100

    add submit event to login user

--- src/modules/pages/Signin.jsx
+++ src/modules/pages/Signin.jsx
@@ -1,32 +1,46 @@
-import { useNavigate } from 'react-router-dom'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../auth/authContext";
 
-/* eslint-disable jsx-a11y/anchor-is-valid */
 const Signin = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const { signIn } = UserAuth();
+  const [error, setError] = useState(null);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const isInvalid = password === "" || email === "";
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setError(null)
+    try {
+      await signIn(email, password)
+      navigate('/home')
+    } catch (err) {
+      setError(err)
+      console.error(err)
+    }
+  }
 
   return (
     <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <img
-            className="mx-auto h-12 w-auto"
-            src="/logo192.png"
-            alt="logo"
-          />
+          <img className="mx-auto h-12 w-auto" src="/logo192.png" alt="logo" />
           <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
             Sign in to your account
           </h2>
           <p className="mt-2 text-center text-sm text-gray-600">
             Or{" "}
-            <a
-              onClick={() => navigate('/signup')}
+            <Link
+              to="/signup"
               className=" cursor-pointer font-medium text-indigo-600 hover:text-indigo-500"
             >
               sign up if you do not have an account
-            </a>
+            </Link>
           </p>
         </div>
-        <form className="mt-8 space-y-6" action="#" method="POST">
+        <form onSubmit={handleSubmit} className="mt-8 space-y-6">
           <input type="hidden" name="remember" defaultValue="true" />
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
@@ -34,10 +48,9 @@ const Signin = () => {
                 Email address
               </label>
               <input
-                id="email-address"
-                name="email"
+                onChange={(e) => setEmail(e.target.value)}
                 type="email"
-                autoComplete="email"
+                value={email}
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
@@ -48,10 +61,9 @@ const Signin = () => {
                 Password
               </label>
               <input
-                id="password"
-                name="password"
+                onChange={(e) => setPassword(e.target.value)}
                 type="password"
-                autoComplete="current-password"
+                value={password}
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
@@ -59,46 +71,27 @@ const Signin = () => {
             </div>
           </div>
 
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                id="remember-me"
-                name="remember-me"
-                type="checkbox"
-                className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
-              />
-              <label
-                htmlFor="remember-me"
-                className="ml-2 block text-sm text-gray-900"
-              >
-                Remember me
-              </label>
-            </div>
-
+          <div className="flex items-center justify-center">
             <div className="text-sm">
-              <a
-                href="#"
+              <Link
+                to="/admin"
                 className="font-medium text-indigo-600 hover:text-indigo-500"
               >
                 Forgot your password?
-              </a>
+              </Link>
             </div>
           </div>
 
           <div>
             <button
               type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              disabled={isInvalid}
+              className="cursor-pointer group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
             >
-              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                {/*<LockClosedIcon
-                  className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
-                  aria-hidden="true"
-  /> */}
-              </span>
               Sign in
             </button>
           </div>
+          {error && <p>{error.message}</p>}
         </form>
       </div>
     </div>
