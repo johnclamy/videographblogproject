f2a3c35ae13fa2caca28b0b80a1fe8a86a70e8e5
John_Lamy <johnclamy@gmail.com>
2022-04-21 16:24:40 +0100

    display todo as primary and secondary text

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,10 @@
 const Todo = ({todo}) => {
   return (
     <li className="border-2 rounded border-gray-200 p-5 mb-1" key={todo}>
-      {todo}
+      <div>
+        <p className="text-xl font-semibold text-blue-900">{todo}</p>
+        <p className="text-sm font-normal text-gray-400">{todo}</p>
+      </div>
     </li>
   );
 }
