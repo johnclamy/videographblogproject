a3eb700e694fbcb3883590bc4f71d25259420ffb
John_Lamy <johnclamy@gmail.com>
2022-04-21 11:15:16 +0100

    render todos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,36 @@
+import { useState } from 'react'
+
+const initTodos = ['walk the dog', 'cook breakfast']
+
 function App() {
+  const [todos, setTodos] = useState(initTodos)
+  const [input, setInput] = useState('')
+
+  const handleAddTodo = e => {
+    e.preventDefault()
+    setTodos([...todos, input])
+    setInput('')
+  }
+
   return (
-    <div>test firebase app</div>
+    <div className="sm:container sm:mx-auto">
+      <div className="max-w-2xl mx-auto">
+        <h1 className="my-3 capitalize text-center text-blue-900 text-3xl font-bold">
+          my todo list
+        </h1>
+        <form>
+          <input value={input} onChange={(e) => setInput(e.target.value)} />
+          <button type="submit" onClick={handleAddTodo}>
+            add todo
+          </button>
+        </form>
+        <ul>
+          {todos.map((todo) => (
+            <li>{todo}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
 
