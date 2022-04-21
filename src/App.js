71cfa2c42cb68e267724a5abfd2c7167185fd687
John_Lamy <johnclamy@gmail.com>
2022-04-21 13:57:26 +0100

    style todo form

--- src/App.js
+++ src/App.js
@@ -18,15 +18,28 @@ function App() {
         <h1 className="my-3 capitalize text-center text-blue-900 text-3xl font-bold">
           my todo list
         </h1>
-        <form>
-          <input value={input} onChange={(e) => setInput(e.target.value)} />
-          <button type="submit" onClick={handleAddTodo}>
-            add todo
-          </button>
-        </form>
-        <ul>
+        <div class="flex h-32 mt-4 w-full items-center justify-center bg-blue-900">
+          <form className="flex rounded bg-white w-[30rem]">
+            <input
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              className="w-full border-none bg-transparent px-4 py-1 text-gray-900 outline-none focus:outline-none"
+              placeholder="write a TODO"
+            />
+            <button
+              type="submit"
+              onClick={handleAddTodo}
+              disabled={!Boolean(input)}
+              className="m-2 rounded px-4 py-2 border-2 bg-white hover:bg-orange-100 border-orange-700 cursor-pointer font-semibold text-orange-700 capitalize"
+            >
+              add
+            </button>
+          </form>
+        </div>
+
+        <ul className='mt-6'>
           {todos.map((todo) => (
-            <li>{todo}</li>
+            <li className='border-2 rounded border-gray-200 p-5 mb-1' key={todo}>{todo}</li>
           ))}
         </ul>
       </div>
