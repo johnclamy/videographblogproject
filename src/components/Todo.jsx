ad9dc9298053c5795c7bd7b74376b6e32a3d600e
John_Lamy <johnclamy@gmail.com>
2022-04-22 07:41:32 +0100

    destructure item prop

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,7 @@
-const Todo = ({todo}) => {
+const Todo = ({ item }) => {
+  const { id, todo } = item
   return (
-    <li className="border-2 rounded border-gray-200 p-5 mb-1" key={todo}>
+    <li className="border-2 rounded border-gray-200 p-5 mb-1" key={id}>
       <div>
         <p className="text-xl font-semibold text-blue-900">{todo}</p>
         <p className="text-sm font-normal text-gray-400">{todo}</p>
