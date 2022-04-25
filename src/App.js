a8831618c658bbdee7cb84a9683df8978b0e859f
John_Lamy <johnclamy@gmail.com>
2022-04-25 16:20:17 +0100

    set custom classes for css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-const App = () => (
-  <div className="flex justify-center flex-col items-center h-screen bg-black">
-    <header className="flex flex-col items-center mb-3">
-      <img height="48px" width="48px" src="/logo192.png" alt="logo" />
-      <h1 className="my-2 capitalize text-center text-xl font-semibold text-white">
-        shorts
-      </h1>
-    </header>
-    <main className="relative h-3/4 w-2/5 min-w-sm bg-white ">
-      videos go here...
-    </main>
-  </div>
-);
+const App = () => {
+  return (
+    <div className="container">
+      <header className="app-header">
+        <img width="48px" src="/logo192.png" alt="logo" />
+        <h1 className="logo-text">shorts</h1>
+      </header>
+      <main className="content">videos go here...</main>
+    </div>
+  )
+}
 
 export default App 
