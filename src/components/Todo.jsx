67f2a41255b13c7343fd0d41b258df5eb6e65fb0
John_Lamy <johnclamy@gmail.com>
2022-04-25 11:51:43 +0100

    add delete todo btn to Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,14 @@
+import DeleteIconButton from "./DeleteIconButton";
+
 const Todo = ({ item }) => {
   const { id, todo } = item
   return (
-    <li className="border-2 rounded border-gray-200 p-5 mb-1" key={id}>
+    <li className=" flex justify-between border-2 rounded border-gray-200 p-5 mb-1" key={id}>
       <div>
         <p className="text-xl font-semibold text-blue-900">{todo}</p>
         <p className="text-sm font-normal text-gray-400">{todo}</p>
       </div>
+      <DeleteIconButton id={id} />
     </li>
   );
 }
