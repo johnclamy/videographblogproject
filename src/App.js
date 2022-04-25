7ecc3d0585df1add86551b3b8ead5d2d22e50a77
John_Lamy <johnclamy@gmail.com>
2022-04-25 15:34:03 +0100

    setup app layout

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
-const App = () => <div>testing the stories app</div>
+const App = () => (
+  <div className="flex justify-center flex-col items-center h-screen bg-black">
+    <header className="flex flex-col items-center mb-3">
+      <img height="48px" width="48px" src="/logo192.png" alt="logo" />
+      <h1 className="my-2 capitalize text-center text-xl font-semibold text-white">
+        shorts
+      </h1>
+    </header>
+    <main className="relative h-3/4 w-2/5 min-w-sm bg-white ">
+      videos go here...
+    </main>
+  </div>
+);
 
 export default App 
