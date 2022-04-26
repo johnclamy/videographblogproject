4f79d322050e140ea9316602427c1115958b9f6b
John_Lamy <johnclamy@gmail.com>
2022-04-26 14:25:40 +0100

    reduce the width of the form on lg screens

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { Container, Row, Col } from 'react-bootstrap'
 import Header from './components/Header'
+import AddTodoForm from './components/AddTodoForm'
 
 const App = () => {
   return (
     <Container>
       <Row>
-        <Col md={{ span: 6, offset: 3 }}>
+        <Col
+          md={{ span: 8, offset: 2 }}
+          lg={{ span: 6, offset: 3 }}
+        >
           <Header />
+          <main className='my-4'>
+            <AddTodoForm />
+          </main>
         </Col>
       </Row>
     </Container>
