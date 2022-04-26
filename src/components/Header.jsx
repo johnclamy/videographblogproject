32e5c334e818fb9cba3394e2a784380a1a94f2fb
John_Lamy <johnclamy@gmail.com>
2022-04-26 23:31:43 +0100

    Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 const Header = () => (
-  <header className="mt-3 mb-2">
+  <header className="mt-3 mb-4">
     <h1 className="h3 text-center text-capitalize font-weight-bolder">my todo list</h1>
   </header>
 )
