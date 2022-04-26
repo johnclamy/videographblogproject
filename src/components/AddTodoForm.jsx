50512693e4d5a6a9de9a34ce6a1a51221c57271f
John_Lamy <johnclamy@gmail.com>
2022-04-26 23:33:21 +0100

    add a todo form

--- src/components/AddTodoForm.jsx
+++ src/components/AddTodoForm.jsx
@@ -2,13 +2,10 @@ import { Button, Form } from 'react-bootstrap'
 
 const AddTodoForm = () => {
   return (
-    <Form className="mt-4">
-      <Form.Group>
-        <Form.Label>Enter todo</Form.Label>
-        <Form.Control className="mb-3" type="text" placeholder="what to do next..." />
-      </Form.Group>
+    <Form className="my-4">
+      <Form.Control className="mb-3" size="lg" type="text" placeholder="what to do next..." />
       <div className="d-grid gap-2">
-        <Button className='text-capitalize' variant="success" size="md">
+        <Button className='mb-4 text-capitalize' variant="success" size="lg">
           submit
         </Button>
       </div>
