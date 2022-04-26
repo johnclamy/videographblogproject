3dc6cc50077cea61db9893e2490b93ef91143b49
John_Lamy <johnclamy@gmail.com>
2022-04-26 11:53:30 +0100

    reset todos app with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 const App = () => {
   return (
-    <div className="container">
-      <header className="app-header">
-        <img width="48px" src="/logo192.png" alt="logo" />
-        <h1 className="logo-text">shorts</h1>
-      </header>
-      <main className="content">videos go here...</main>
+    <div>
+      testing app component
     </div>
   )
 }
