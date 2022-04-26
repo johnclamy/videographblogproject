c4b908e85409c2a03452396164b42b0554c17eca
John_Lamy <johnclamy@gmail.com>
2022-04-26 13:52:07 +0100

    Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Container, Row, Col } from 'react-bootstrap'
+import Header from './components/Header'
+
 const App = () => {
   return (
-    <div>
-      testing app component
-    </div>
-  )
+    <Container>
+      <Row>
+        <Col md={{ span: 6, offset: 3 }}>
+          <Header />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default App 
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+const Header = () => (
+  <header className="mt-3 mb-2">
+    <h1 className="h3 text-center text-capitalize font-weight-bolder">my todo list</h1>
+  </header>
+)
+
+export default Header
