23eb0307141aca1d7c960802ac6c7f8c561b7c81
John_Lamy <johnclamy@gmail.com>
2022-04-27 15:38:37 +0100

    fetch data in TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,27 @@
-import { ListGroup } from 'react-bootstrap'
+import { useEffect, useState } from 'react'
+import { Alert, ListGroup } from 'react-bootstrap'
 import TodoItem from './TodoItem'
-
-const todos = [
-  { id: '1', todo: 'walk the dog' },
-  { id: '2', todo: 'cook breakfast' },
-  { id: '3', todo: 'read a book' }
-]
+import { fetchData } from '../database'
 
 const TodoList = () => {
-  return (
-    <ListGroup className='mt-4'>
-      {todos.map(todoItem => <TodoItem key={todoItem.id} item={todoItem} />)}
+  const [todos, setTodos] = useState([])
+  const renderTodos = !todos.length ? (
+    <Alert className="text-center" variant="info">
+      You do not have any TODOS at present
+    </Alert>
+  ) : (
+    <ListGroup className="mt-4">
+      {todos.map((todoItem) => (
+        <TodoItem key={todoItem.id} item={todoItem} />
+      ))}
     </ListGroup>
   );
+
+  useEffect(() => {
+    fetchData(setTodos, 'todos')
+  }, [])
+
+  return renderTodos
 }
 
 export default TodoList
