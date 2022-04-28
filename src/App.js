8d1b3ffda5b633fe47015f020b6fa0a7d02c6603
John_Lamy <johnclamy@gmail.com>
2022-04-28 13:12:59 +0100

    use wrapper css class to center content

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import './App.css'
 const App = () => {
   return (
     <Container className="h-100">
-      <Row className="h-100">
+      <Row className="h-100 wrapper">
         <Col
-          className="h-100"
+          className="h-100 mb-4 d-sm-flex flex-column justify-content-start align-items-center"
           md={{ span: 8, offset: 2 }}
           lg={{ span: 6, offset: 3 }}
         >
-          <main className="h-100 mb-4 d-sm-flex justify-content-center align-items-end">
-            <header className="mb-4 text-center">
-              <img src="/logo192.png" width="48" alt="app logo" />
-              <h1 className=" mt-1 text-capitalize h3">shorts</h1>
-            </header>
-            <article>
-              videos go here...
-            </article>
-          </main>
+          <header className="mb-4 text-center">
+            <img src="/logo192.png" width="48" alt="app logo" />
+            <h1 className=" mt-1 text-capitalize h3">shorts</h1>
+          </header>
+          <article>videos go here...</article>
         </Col>
       </Row>
     </Container>
