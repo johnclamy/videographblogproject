54446568269a6414c2745f294af6c4b976322c82
John_Lamy <johnclamy@gmail.com>
2022-04-28 12:52:47 +0100

    setup basic structure for shorts app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import { Container, Row, Col } from 'react-bootstrap'
+import './App.css'
 
 const App = () => {
   return (
-    <Container>
-      <Row>
+    <Container className="h-100">
+      <Row className="h-100">
         <Col
+          className="h-100"
           md={{ span: 8, offset: 2 }}
           lg={{ span: 6, offset: 3 }}
         >
-          <h1>Shorts app</h1>
+          <main className="h-100 mb-4 d-sm-flex justify-content-center align-items-end">
+            <header className="mb-4 text-center">
+              <img src="/logo192.png" width="48" alt="app logo" />
+              <h1 className=" mt-1 text-capitalize h3">shorts</h1>
+            </header>
+            <article>
+              videos go here...
+            </article>
+          </main>
         </Col>
       </Row>
     </Container>
--- src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-const Header = () => (
-  <header className="mt-3 mb-4">
-    <h1 className="h3 text-center text-capitalize font-weight-bolder">my todo list</h1>
-  </header>
-)
-
-export default Header
