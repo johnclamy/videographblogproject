ed16755b3b868039353901b6606c955bd245406d
John_Lamy <johnclamy@gmail.com>
2022-05-04 16:26:37 +0100

    setup user auth service from firebase

---
+++ src/firebase/auth/index.js
@@ -0,0 +1,59 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState
+} from 'react'
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from 'firebase/auth'
+import app from '../config'
+
+const UserContext = createContext()
+const auth = getAuth(app)
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState({})
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
+  const signin = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+
+  const logout = () => {
+    return signOut(auth)
+  }
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      console.log(currentUser)
+      setUser(currentUser)
+    })
+
+    return () => {
+      unsubscribe()
+    }
+  }, [])
+
+  return (
+    <UserContext.Provider value={{
+      createUser,
+      user,
+      logout,
+      signin
+    }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export const UserAuth = () => {
+  return useContext(UserContext)
+}
