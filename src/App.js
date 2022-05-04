0a323784d668d7b8215cf5ef227a2e921b1dc87a
John_Lamy <johnclamy@gmail.com>
2022-05-04 18:43:57 +0100

    wrap App component with AuthContextProvider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Container, Row, Col } from 'react-bootstrap'
 import { Routes, Route } from 'react-router-dom'
+import AuthContextProvider from './firebase/auth'
 import NavigationBar from './components/layout/NavigationBar'
 import SigninPage from './components/pages/Signin'
 import SignupPage from './components/pages/Signup'
@@ -11,12 +12,14 @@ const App = () => {
     <Container fluid>
       <Row>
         <Col md={{ span: 10, offset: 1 }} lg={{ span: 6, offset: 3 }}>
-          <NavigationBar />
-          <Routes>
-            <Route path='/' element={<SigninPage />} />
-            <Route path='/signup' element={<SignupPage />} />
-            <Route path='/home' element={<HomePage />} />
-          </Routes>
+          <AuthContextProvider>
+            <NavigationBar />
+            <Routes>
+              <Route path="/" element={<SigninPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/home" element={<HomePage />} />
+            </Routes>
+          </AuthContextProvider>
         </Col>
       </Row>
     </Container>
