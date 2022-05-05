87f354a857a000ac9979ea07e589f7f9eb7192f9
John_Lamy <johnclamy@gmail.com>
2022-05-05 13:29:59 +0100

    use createUser() in SignupForm.jsx

--- src/components/layout/SignupForm.jsx
+++ src/components/layout/SignupForm.jsx
@@ -1,20 +1,40 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
 import { errorAlert } from '../helper'
+import { UserAuth } from '../../firebase/auth'
 
 const SignupForm = () => {
   const [email, setEmail] = useState('')
   const [passwordOne, setPasswordOne] = useState('')
   const [passwordTwo, setPasswordTwo] = useState('');
   const [error, setError] = useState(null)
+  const { createUser } = UserAuth()
+  const navigate = useNavigate()
 
   const isInvalid =
     passwordOne !== passwordTwo ||
     passwordOne === '' ||
     email === ''
 
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    setError(null)
+
+    try {
+      await createUser(email, passwordOne)
+      navigate('/home')
+      setEmail('')
+      setPasswordOne('')
+      setPasswordTwo('')
+    } catch (err) {
+      setError(err)
+      console.log(err.message)
+    }
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3">
         <Form.Label>Email address</Form.Label>
         <Form.Control
