bab65dbcfdcb13b041f33201b04e10cc12a92bdd
John_Lamy <johnclamy@gmail.com>
2022-05-05 23:33:29 +0100

    wrap the home page with a protected route

--- src/components/routes/index.jsx
+++ src/components/routes/index.jsx
@@ -2,12 +2,20 @@ import { Routes, Route } from 'react-router-dom'
 import SigninPage from '../pages/Signin'
 import SignupPage from "../pages/Signup";
 import HomePage from "../pages/Home";
+import ProtectedRoute from './Protected'
 
 const AppRoutes = () => (
   <Routes>
     <Route path="/" element={<SigninPage />} />
     <Route path="/signup" element={<SignupPage />} />
-    <Route path="/home" element={<HomePage />} />
+    <Route
+      path="/home"
+      element={
+        <ProtectedRoute>
+          <HomePage />
+        </ProtectedRoute>
+      }
+    />
   </Routes>
 );
 
