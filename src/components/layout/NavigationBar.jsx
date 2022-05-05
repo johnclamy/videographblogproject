3fed4e6e9ecd9a123d20f187c3d518515247d7f9
John_Lamy <johnclamy@gmail.com>
2022-05-05 13:33:47 +0100

    add AuthButton component to NavigationBar.jsx

--- src/components/layout/NavigationBar.jsx
+++ src/components/layout/NavigationBar.jsx
@@ -1,20 +1,18 @@
-// import { Link } from 'react-router-dom'
-import { Container, Navbar } from 'react-bootstrap'
+import { Container, Navbar } from "react-bootstrap";
+import AuthButton from './AuthButton'
 
-const NavigationBar = () => {
-  return (
-    <Navbar>
-      <Container>
-        <Navbar.Brand className="text-uppercase" href="#home">
-          my todos
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text className='text-capitalize'></Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
+const NavigationBar = () => (
+  <Navbar>
+    <Container>
+      <Navbar.Brand className="text-uppercase" href="#home">
+        my todos
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end">
+        <AuthButton />
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+)
 
-export default NavigationBar
+export default NavigationBar;
