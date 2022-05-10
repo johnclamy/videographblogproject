f27940665a4e44291821094d7c4e655baaf8001d
John_Lamy <johnclamy@gmail.com>
2022-05-10 16:12:06 +0100

    setup the main content header in Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Row, Col } from 'react-bootstrap'
 import AddButton from '../layout/AddButton'
 import Toolbar from '../layout/Toolbar'
 
@@ -5,7 +6,11 @@ const Home = () => {
   return (
     <main>
       <header className="mt-4 mb-2">
-        <AddButton />
+        <Row>
+          <Col sm={{ span: 6, offset: 1 }}>
+            <AddButton />
+          </Col>
+        </Row>
         <Toolbar />
       </header>
     </main>
