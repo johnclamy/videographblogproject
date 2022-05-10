4157b12c07a2de2fc35895ccb1fb395619328d20
John_Lamy <johnclamy@gmail.com>
2022-05-10 14:41:31 +0100

    setup contant values for bootstrap btn styling

---
+++ src/constants/btn-variants.js
@@ -0,0 +1,7 @@
+const BUTTON_VARIANTS = [
+  { variant: 'warning', text: 'Search' },
+  { variant: 'info', text: 'Export JSON' },
+  { variant: 'dark', text: 'Export CSV' }
+]
+
+export default BUTTON_VARIANTS
