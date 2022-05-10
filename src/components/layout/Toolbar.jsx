f33c093e0a2b7a2b89441dc5a168e58c89ed802a
John_Lamy <johnclamy@gmail.com>
2022-05-10 15:48:01 +0100

    reset the margin to the header tag of Home.jsx

--- src/components/layout/Toolbar.jsx
+++ src/components/layout/Toolbar.jsx
@@ -2,7 +2,7 @@ import Button from 'react-bootstrap/Button'
 import BUTTON_VARIANTS from '../../constants/btn-variants'
 
 const Toolbar = () => (
-  <section className="mt-4 mb-2 d-flex justify-content-evenly">
+  <section className="d-flex justify-content-evenly">
     {BUTTON_VARIANTS.map((btn, idx) => (
       <Button style={{width: '95px'}} key={idx} size='sm' variant={`outline-${btn.variant}`}>
         {btn.text}
