c8b3d3069b5268916b45ddade00f79b5d205bc1b
John_Lamy <johnclamy@gmail.com>
2022-05-10 23:35:59 +0100

    data to be render on table

---
+++ src/components/app/initialTodos.js
@@ -0,0 +1,25 @@
+const initialTodos = [
+  {
+    id: "1",
+    task: "walk the dog",
+    isCompleted: false,
+    startDate: null,
+    isImportant: true,
+  },
+  {
+    id: "2",
+    task: "cook breakfast",
+    isCompleted: false,
+    startDate: null,
+    isImportant: true,
+  },
+  {
+    id: "3",
+    task: "go food shopping",
+    isCompleted: false,
+    startDate: null,
+    isImportant: false,
+  },
+];
+
+export default initialTodos
