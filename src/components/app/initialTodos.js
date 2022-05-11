e0af0e4014fe8c202c7c5791c274e33cd25b1afd
John_Lamy <johnclamy@gmail.com>
2022-05-11 16:13:18 +0100

    set default dates

--- src/components/app/initialTodos.js
+++ src/components/app/initialTodos.js
@@ -3,22 +3,22 @@ const initialTodos = [
     id: "1",
     task: "walk the dog",
     isCompleted: false,
-    startDate: null,
-    isImportant: true,
+    startDate: new Date("2022-05-05T20:24:17").toString(),
+    isUrgent: true,
   },
   {
     id: "2",
     task: "cook breakfast",
     isCompleted: false,
-    startDate: null,
-    isImportant: true,
+    startDate: new Date("2022-05-05T21:07:03").toString(),
+    isUrgent: true,
   },
   {
     id: "3",
     task: "go food shopping",
     isCompleted: false,
-    startDate: null,
-    isImportant: false,
+    startDate: new Date("1995-05-05T21:22:14").toString(),
+    isUrgent: false,
   },
 ];
 
