e7b2e9fe8c71e078c8b4be0a02599d7cb883c3f5
John_Lamy <johnclamy@gmail.com>
2022-05-12 13:35:27 +0100

    pass todos array down as prop in AppleTable.jsx

--- src/components/layout/AppTable.jsx
+++ src/components/layout/AppTable.jsx
@@ -1,10 +1,12 @@
 import Table from 'react-bootstrap/Table'
 import TableHeader from './TableHeader'
+import TableBody from './TableBody'
 
-const AppTable = () => {
+const AppTable = ({ todos }) => {
   return (
     <Table className="mt-2 mb-4" responsive striped bordered hover size="sm">
       <TableHeader />
+      <TableBody todos={todos} />
     </Table>
   );
 }
