4bf79f60653abfe0dc39edc5af8be372546d1773
John_Lamy <johnclamy@gmail.com>
2022-05-16 13:51:41 +0100

    remove todos prop from AppTable.jsx

--- src/components/layout/AppTable.jsx
+++ src/components/layout/AppTable.jsx
@@ -2,11 +2,11 @@ import Table from 'react-bootstrap/Table'
 import TableHeader from './TableHeader'
 import TableBody from './TableBody'
 
-const AppTable = ({ todos }) => {
+const AppTable = () => {
   return (
     <Table className="mt-2 mb-4" responsive striped bordered hover size="sm">
       <TableHeader />
-      <TableBody todos={todos} />
+      <TableBody />
     </Table>
   );
 }
