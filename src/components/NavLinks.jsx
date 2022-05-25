de9632264e61dbb1c78bcba174f8b06904385986
John_Lamy <johnclamy@gmail.com>
2022-05-25 23:12:09 +0100

    setup navigation links in NavLinks.jsx

---
+++ src/components/NavLinks.jsx
@@ -0,0 +1,10 @@
+const NavLinks = () => {
+  return (
+    <div className="nav-links">
+      <a href="/home">Home</a>
+      <a href="/rooms">Rooms</a>
+    </div>
+  );
+}
+
+export default NavLinks
