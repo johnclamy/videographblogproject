4256c854443b2d53e161d148daee9571c52f2fae
John_Lamy <johnclamy@gmail.com>
2022-05-25 23:17:02 +0100

    add Navbar to App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,19 @@
 import { Container, Row, Col } from 'react-bootstrap'
+import Navbar from '../components/Navbar'
 import './App.css'
 
 const App = () => {
   return (
-    <Container fluid>
-      <Row>
-        <Col md={{ span: 10, offset: 1 }} lg={{ span: 6, offset: 3 }}>
-          <p>testing the app component</p>
-        </Col>
-      </Row>
-    </Container>
+    <>
+      <Navbar />
+      <Container>
+        <Row>
+          <Col md={{ span: 10, offset: 1 }} lg={{ span: 6, offset: 3 }}>
+            content goes here...
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
