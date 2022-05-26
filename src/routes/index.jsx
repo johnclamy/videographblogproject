df044511979fc02755307add5d6f1113e76ef661
John_Lamy <johnclamy@gmail.com>
2022-05-26 15:21:16 +0100

    setup the app routes

---
+++ src/routes/index.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import * as ROUTES from "../routes/constants";
+import HomePage from '../pages/Home'
+import RoomsPage from '../pages/Rooms'
+import SignUpPage from '../pages/Signup'
+import SignInPage from '../pages/Signin'
+
+const AppRoutes = () => {
+  <Routes>
+    <Route path={ROUTES.HOME} element={<HomePage />} />
+    <Route path={ROUTES.ROOMS} element={<RoomsPage />} />
+    <Route path={ROUTES.SIGN_UP} element={<SignUpPage />} />
+    <Route path={ROUTES.SIGN_IN} element={<SignInPage />} />
+  </Routes>;
+}
+
+export default AppRoutes
