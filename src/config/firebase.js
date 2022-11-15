fd7dd75704c10dfe3959141d1f6946b442886942
John_Lamy <johnclamy@gmail.com>
2022-11-15 22:49:45 +0000

    setup the firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAVF4qzgS2WBprnDw66MPbd12bz0BPpLm0",
+  authDomain: "todo-app-4b9fc.firebaseapp.com",
+  projectId: "todo-app-4b9fc",
+  storageBucket: "todo-app-4b9fc.appspot.com",
+  messagingSenderId: "474858714454",
+  appId: "1:474858714454:web:88e141aec2e18bcd4852f7",
+};
+
+export default firebaseConfig;
