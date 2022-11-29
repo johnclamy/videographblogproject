33f974aacbe4731d03e927595be1f3f185b1e6dc
John_Lamy <johnclamy@gmail.com>
2022-11-29 18:03:50 +0000

    create the navbar

---
+++ src/components/Authbar.js
@@ -0,0 +1,29 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavScrollExample() {
+  const email = "john.doe@abcd-inc.com";
+
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#">Blogster</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+          ></Nav>
+          <Navbar.Text style={{marginRight: '1rem'}}>{email}</Navbar.Text>
+          <Button size="sm" variant="outline-info">
+            Logout
+          </Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavScrollExample;
