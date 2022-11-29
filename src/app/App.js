a13b604470f6ea2a585bbefaa5bdf0375dfbea2b
John_Lamy <johnclamy@gmail.com>
2022-11-29 22:11:55 +0000

    remove unwanted components

--- src/app/App.js
+++ src/app/App.js
@@ -1,12 +1,12 @@
 import Wrapper from "../components/Wrapper";
 import Authbar from "../components/Authbar"
+import PostForm from '../components/PostForm'
 
 const App = () => (
-  <>
-    <Wrapper>
-      <Authbar />
-    </Wrapper>
-  </>
+  <Wrapper>
+    <Authbar />
+    <PostForm />
+  </Wrapper>
 );
 
 export default App;
