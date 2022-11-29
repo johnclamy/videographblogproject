3f5966f92fc9fe5edbbc77a0a6c661b6f68dda35
John_Lamy <johnclamy@gmail.com>
2022-11-29 22:10:38 +0000

    add a form to add post content

---
+++ src/components/PostForm.js
@@ -0,0 +1,35 @@
+import { Button, Container, Form } from 'react-bootstrap'
+
+const PostForm = () => {
+  return (
+    <Container className="border border-info rounded m-1">
+      <header className="mt-3">
+        <h1 className="font-weight-bold text-info text-uppercase mb-0 h4">
+          add post
+        </h1>
+        <p>
+          author: <b>John Doe</b>
+        </p>
+      </header>
+      <Form className="mb-4">
+        <Form.Group className="mb-3">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Post title</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Add post contrent</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <Button variant="info" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
+}
+
+export default PostForm
