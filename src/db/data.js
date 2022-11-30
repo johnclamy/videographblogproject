8cb6dc070bde67ce722f748e942aed2d0b13babf
John_Lamy <johnclamy@gmail.com>
2022-11-30 11:56:43 +0000

    place the data content in its own folder

---
+++ src/db/data.js
@@ -0,0 +1,24 @@
+const data = {
+  user: {
+    name: "John Doe",
+    email: "john.doe@abcd-inc.com",
+    password: "swordfish",
+  },
+
+  posts: [
+    {
+      title: "React Hooks",
+      author: "John Doe",
+      content:
+        "The greatest thing since sliced bread! the adoption is going on a rapid speed as developer are loving it!!",
+    },
+    {
+      title: "Will React Native survive?",
+      author: "Betsy Deolallie",
+      content:
+        "React Native has had a battle of sorts lately. Its up against a plethora of new comers in the 'cross-platform development arena.'",
+    },
+  ],
+};
+
+export default data
