9a48ac815ca4a0fe25d49670f66f68889595a38a
John_Lamy <johnclamy@gmail.com>
2022-11-30 11:59:47 +0000

    import data and pass it down to relevant component

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,14 @@
 import Wrapper from "../components/Wrapper";
 import Authbar from "../components/Authbar"
 import PostForm from '../components/PostForm'
+import PostList from '../post/PostList';
+import data from '../db/data';
 
 const App = () => (
   <Wrapper>
-    <Authbar />
-    <PostForm />
+    <Authbar email={data.user.email} />
+    <PostForm author={data.user.name} />
+    <PostList posts={data.posts} />
   </Wrapper>
 );
 
