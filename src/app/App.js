8bbd039a28cbda81d1654c8fa134d42f8368dbdc
John_Lamy <johnclamy@gmail.com>
2022-11-30 18:01:43 +0000

    add the accordion component to App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,13 @@
 import Wrapper from "../components/Wrapper";
-import Authbar from "../components/Authbar"
-import PostForm from '../components/PostForm'
-import PostList from '../post/PostList';
-import data from '../db/data';
+import Authbar from "../components/Authbar";
+import PostList from "../post/PostList";
+import data from "../db/data";
+import PostFormAccordion from "../components/PostFormAccordion";
 
 const App = () => (
   <Wrapper>
-    <Authbar email={data.user.email} />
-    <PostForm author={data.user.name} />
+    <Authbar email="" /*{data.user.email}*/ />
+    <PostFormAccordion author={data.user.name} />
     <PostList posts={data.posts} />
   </Wrapper>
 );
