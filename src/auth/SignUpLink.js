6395ace1507efeb947acc32d99c47a0a0d200708
John_Lamy <johnclamy@gmail.com>
2022-12-02 13:41:02 +0000

    send signUp action creator to dispatch for signup

--- src/auth/SignUpLink.js
+++ src/auth/SignUpLink.js
@@ -1,14 +1,31 @@
 import { useState } from "react";
+import { signUp } from '../global/actions/action-creator'
 import { Button, Form, Modal } from "react-bootstrap";
 
-const SignUpLink = () => {
-  const [show, setShow] = useState(false)
-  const handleClose = () => setShow(false)
-  const handleShow = () => setShow(true)
+const SignUpLink = ({ dispatch }) => {
+  const [show, setShow] = useState(false);
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [passwordOne, setPasswordOne] = useState('')
+  const [passwordTwo, setPasswordTwo] = useState("");
 
-  const handleSubmit = e => {
-    e.preventDefault()
-  }
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const isInvalid =
+    passwordOne !== passwordTwo ||
+    passwordOne === "" ||
+    email === "" ||
+    username === "";
+
+  const handleClick = () => {
+    dispatch(signUp(username, email, passwordOne));
+    setUsername('')
+    setEmail('')
+    setPasswordOne('')
+    setPasswordTwo('')
+    handleClose()
+  };
 
   return (
     <>
@@ -23,22 +40,40 @@ const SignUpLink = () => {
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form onSubmit={handleSubmit} className="mb-4">
+          <Form className="mb-4">
             <Form.Group className="mb-3">
               <Form.Label>Username</Form.Label>
-              <Form.Control type="text" placeholder="ex: John Doe" />
+              <Form.Control
+                type="text"
+                placeholder="ex: John Doe"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="name@example.com" />
+              <Form.Control
+                type="email"
+                value={email}
+                placeholder="name@example.com"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" />
+              <Form.Control
+                type="password"
+                value={passwordOne}
+                onChange={(e) => setPasswordOne(e.target.value)}
+              />
             </Form.Group>
             <Form.Group className="mb-3">
               <Form.Label>Confirm password</Form.Label>
-              <Form.Control type="password" />
+              <Form.Control
+                type="password"
+                value={passwordTwo}
+                onChange={(e) => setPasswordTwo(e.target.value)}
+              />
             </Form.Group>
           </Form>
         </Modal.Body>
@@ -46,13 +81,13 @@ const SignUpLink = () => {
           <Button variant="outline-info" type="button" onClick={handleClose}>
             Cancel
           </Button>
-          <Button variant="info" type="submit">
+          <Button variant="info" type="button" disabled={isInvalid} onClick={handleClick}>
             Sign Up
           </Button>
         </Modal.Footer>
       </Modal>
     </>
   );
-}
+};
 
 export default SignUpLink;
