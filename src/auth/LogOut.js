6931297783505a73abb2a7aa2272ab0470cd4fda
John_Lamy <johnclamy@gmail.com>
2022-12-02 13:19:55 +0000

    dispatch the logout action creator in LogOut.js

--- src/auth/LogOut.js
+++ src/auth/LogOut.js
@@ -1,10 +1,18 @@
 import { Navbar, Button } from 'react-bootstrap'
+import { logout } from '../global/actions/action-creator'
 
-const LogOut = ({ email }) => {
+const LogOut = ({ email, dispatch }) => {
+  console.log('logout', email, dispatch)
   return (
     <>
       <Navbar.Text style={{marginRight: '1rem'}}>{email}</Navbar.Text>
-      <Button size="sm" variant="outline-info">Logout</Button>
+      <Button
+        size="sm"
+        variant="outline-info"
+        onClick={() => dispatch(logout())}
+      >
+        Logout
+      </Button>
     </>
   )
 }
