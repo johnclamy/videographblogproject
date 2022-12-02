35d7c52a2c66c17ab0a855ca73ad860d676c2d50
John_Lamy <johnclamy@gmail.com>
2022-12-02 22:32:18 +0000

    create a reducer for posts

---
+++ src/global/reducers/postsReducer.js
@@ -0,0 +1,15 @@
+import actionType from "../actions/action-type";
+
+export default function postsReducer(state, { type, payload }) {
+  switch (type) {
+    case actionType.CREATE_POST:
+      const post = {
+        title: payload.title,
+        content: payload.content,
+        author: payload.author,
+      };
+      return [post, ...state];
+    default:
+      throw new Error();
+  }
+}
