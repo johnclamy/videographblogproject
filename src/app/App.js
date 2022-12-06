67ee71e552d31795a463afe52ce5df1fddea5ce6
John_Lamy <johnclamy@gmail.com>
2022-12-06 14:23:41 +0000

    wrap the app with ThemeContextProvider component

--- src/app/App.js
+++ src/app/App.js
@@ -4,12 +4,12 @@ import Wrapper from "../components/Wrapper";
 import Authbar from "../components/Authbar";
 import PostList from "../post/PostList";
 import PostFormAccordion from "../components/PostFormAccordion";
+import ThemeContextProvider from "../contexts/ThemeContext";
 import data from "../db/data";
 
-const defaultData = { user: {}, posts: data.posts }
+const defaultData = { user: {}, posts: data.posts } 
 
 const App = () => {
-
   const [state, dispatch] = useReducer(reducer, defaultData)
   const { user, posts } = state
 
@@ -21,11 +21,13 @@ const App = () => {
   }, [user.username])
 
   return (
-    <Wrapper>
-      <Authbar user={user} dispatch={dispatch} />
-      <PostFormAccordion user={user} dispatch={dispatch} />
-      <PostList posts={posts} />
-    </Wrapper>
+    <ThemeContextProvider>
+      <Wrapper>
+        <Authbar user={user} dispatch={dispatch} />
+        <PostFormAccordion user={user} dispatch={dispatch} />
+        <PostList posts={posts} />
+      </Wrapper>
+    </ThemeContextProvider>
   );
 }
 
