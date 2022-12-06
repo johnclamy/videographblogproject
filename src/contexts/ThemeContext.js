a85360ac87c73ff09e4d08b3f7528cda5c54f35e
John_Lamy <johnclamy@gmail.com>
2022-12-06 12:41:09 +0000

    setup an app theme - light and dark

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,3 +1,21 @@
 import { createContext } from "react";
 
-export const ThemeContext = createContext({})
+const ThemeContext = createContext({
+  isLightTheme: true,
+  light: {
+    bg: "light",
+    variant: "info",
+    textHeading: "text-info",
+    textContent: "text-dark",
+    border: "border border-info",
+  },
+  dark: {
+    bg: "dark",
+    variant: "warning",
+    textHeading: "text-warning",
+    textContent: "text-white",
+    border: "border border-warning",
+  },
+});
+
+export default ThemeContext
