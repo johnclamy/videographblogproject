f7474bf43dd867e9a5556be180cdf2d9d9574418
John_Lamy <johnclamy@gmail.com>
2023-01-18 00:41:07 +0000

    create the login form

---
+++ src/auth/Login.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import { Button, Form } from "react-bootstrap";
+
+const Login = ({ onClose }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(username, password);
+    setUsername('')
+    setPassword('')
+    onClose()
+  }
+  
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3">
+        <Form.Label>Username</Form.Label>
+        <Form.Control
+          type="text"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+    </Form>
+  );
+};
+
+export default Login;
