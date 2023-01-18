2813c549a9ce15e8140dfc8d99ef0cdc285512aa
John_Lamy <johnclamy@gmail.com>
2023-01-18 09:56:00 +0000

    style the b tag text in Logout.jsx

--- src/auth/Logout.jsx
+++ src/auth/Logout.jsx
@@ -3,10 +3,12 @@ import Button from 'react-bootstrap/Button'
 const Logout = ({ user }) => {
   return (
     <section>
-      <b>Logged in as: {user} </b>
-      <Button variant='secondary'>Logout</Button>
+      <b className="fs-6 fst-italic">
+        <span className="fw-normal">Logged in as:</span> {user}{" "}
+      </b>
+      <Button variant="secondary">Logout</Button>
     </section>
-  )
+  );
 }
 
 export default Logout
