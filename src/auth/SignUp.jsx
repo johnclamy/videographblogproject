e5d101d6e50556450921ec65bad060fd863394e9
John_Lamy <johnclamy@gmail.com>
2023-01-18 12:55:14 +0000

    add input forms to SignUp.jsx

--- src/auth/SignUp.jsx
+++ src/auth/SignUp.jsx
@@ -1,3 +1,50 @@
-const SignUp = () => {}
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
-export default SignUp
+const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    setUsername("");
+    setPassword("");
+    // onClose();
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3">
+        <Form.Label>Username</Form.Label>
+        <Form.Control
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3">
+        <Form.Label>Confirm password</Form.Label>
+        <Form.Control
+          type="password"
+          value={passwordConfirm}
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Sign Up
+      </Button>
+    </Form>
+  );
+};
+
+export default SignUp;
