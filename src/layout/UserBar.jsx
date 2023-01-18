4f1256c95c550e08f3deb8ce0ee4019f41daa5c5
John_Lamy <johnclamy@gmail.com>
2023-01-18 12:56:13 +0000

    add the SignUp modal to UserBar.jsx

--- src/layout/UserBar.jsx
+++ src/layout/UserBar.jsx
@@ -1,35 +1,67 @@
 import { useState } from "react";
 import { Button, Modal } from "react-bootstrap";
+import SignUp from '../auth/SignUp'
 import Login from '../auth/Login'
 import Logout from '../auth/Logout'
 
 const UserBar = () => {
-  const [show, setShow] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [showSignUp, setShowSignUp] = useState(false);
   const [user, setUser] = useState('')
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const handleCloseLogin = () => setShowLogin(false);
+  const handleShowLogin = () => setShowLogin(true);
+  const handleCloseSignUp = () => setShowSignUp(false);
+  const handleShowSignUp = () => setShowSignUp(true);
 
   const renderUser = user ? (
     <Logout user={user} />
   ) : (
-    <Button variant="outline-primary" onClick={handleShow}>
-      Login
-    </Button>
-  );
-
+    <>
+      <Button
+        type="button"
+        variant="link"
+        className="btn btn-link text-decoration-none"
+        onClick={handleShowSignUp}
+      >
+        SIGN UP
+      </Button>{" "}
+      <Button
+        type="button"
+        variant="link"
+        className="btn btn-link text-decoration-none"
+        onClick={handleShowLogin}
+      >
+        LOGIN
+      </Button>
+    </>
+  ); 
+ 
   return (
     <>
-      <section className="d-flex">
-        {renderUser}
-      </section>
-      <Modal show={show} onHide={handleClose}>
+      <section className="d-flex">{renderUser}</section>
+      <Modal show={showLogin} onHide={handleCloseLogin}>
         <Modal.Header closeButton>
           <Modal.Title>Login</Modal.Title>
         </Modal.Header>
-        <Modal.Body><Login onClose={handleClose} /></Modal.Body>
+        <Modal.Body>
+          <Login onClose={handleCloseLogin} />
+        </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={handleCloseLogin}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal show={showSignUp} onHide={handleCloseSignUp}>
+        <Modal.Header closeButton>
+          <Modal.Title>Sign Up</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <SignUp onClose={handleCloseSignUp} />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseSignUp}>
             Close
           </Button>
         </Modal.Footer>
