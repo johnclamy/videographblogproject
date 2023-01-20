1ffebe16320a2d3eec95b09f0e05a2d01f63eefe
John_Lamy <johnclamy@gmail.com>
2023-01-20 08:42:27 +0000

    setup the postsReducer

---
+++ src/redux/reducer/postsReducer.js
@@ -0,0 +1,17 @@
+import actionType from "../action/action-type";
+
+export default function postsReducer(state, action) {
+  switch (action.type) {
+    case actionType.CREATE_POST:
+      const newPost = {
+        title: action.title,
+        content: action.content,
+        author: action.author,
+        id: "710b962e-041c-11e1-9234-0123456789ab",
+      };
+      return [newPost, ...state]
+    
+    default:
+      return state
+  }
+}
