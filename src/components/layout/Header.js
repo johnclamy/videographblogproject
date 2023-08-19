8ead49ea0e6547f7f96631eabd5eb110614b858a
John_Lamy <johnclamy@gmail.com>
2023-08-19 23:33:06 +0100

    setup the header component for the main page

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,14 +1,14 @@
 export default function Header() {
   return (
     <header className="w-full container mx-auto">
-      <div className="flex flex-col items-center py-12">
+      <div className="flex flex-col items-center pt-6 pb-12">
         <a
           className="font-bold capitalize text-gray-800 hover:text-gray-700 text-5xl"
           href="/"
         >
           crypto blog
         </a>
-        <p className="text-lg text-gray-600">Lorem Ipsum Dolor Sit Amet</p>
+        <p className="mt-1 text-lg text-gray-600">Lorem Ipsum Dolor Sit Amet</p>
       </div>
     </header>
   );
