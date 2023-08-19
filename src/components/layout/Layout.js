c912d410753fcd7b47268a3203ff5e5f2d024d8c
John_Lamy <johnclamy@gmail.com>
2023-08-19 15:46:20 +0100

    add the Header to Layout component

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,9 +1,11 @@
 import Navbar from "./Navbar"
+import Header from "./Header"
 
 export default function Layout({ children }) {
   return (
     <div className="bg-white">
       <Navbar />
+      <Header />
       {children}
     </div>
   )
