7b1ecf709b442d329e042df512507a3c02e48944
John_Lamy <johnclamy@gmail.com>
2023-08-19 23:27:54 +0100

    setup a sign in btn

---
+++ src/components/widgets/Auth.js
@@ -0,0 +1,9 @@
+export default function Auth() {
+  return (
+    <button class="relative inline-flex items-center justify-center p-0.5 ml-2 mt-4 overflow-hidden text-sm font-medium text-cyan-900 rounded-lg group bg-gradient-to-br from-cyan-600 to-cyan-800 group-hover:from-cyan-500 group-hover:to-gray-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+      <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+        Register/ Sign In
+      </span>
+    </button>
+  );
+}
