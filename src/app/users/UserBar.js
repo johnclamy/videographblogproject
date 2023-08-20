ee5df5ff6e171bc71c1bc89b954e232d4c37549c
John_Lamy <johnclamy@gmail.com>
2023-08-20 23:36:30 +0100

    render a btn or user info in UserBar.js

--- src/app/users/UserBar.js
+++ src/app/users/UserBar.js
@@ -1,5 +1,13 @@
+import { useState } from "react";
+import Auth from "../../components/widgets/Auth" 
+import UserAuthDetails from "./UserAuthDetails";
+
 export default function UserBar() {
+  const [signedIn, setSignedIn] = useState(false);
+
   return (
-    <div>Contains all the user components</div>
+    <div>
+      {signedIn ? <UserAuthDetails /> : <Auth />}
+    </div>
   )
 }
