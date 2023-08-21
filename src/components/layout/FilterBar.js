304df2c6ed27c3d16f8ca46e2d1f7c4045f32174
John_Lamy <johnclamy@gmail.com>
2023-08-21 23:42:57 +0100

    rename the Auth.js component

---
+++ src/components/layout/FilterBar.js
@@ -0,0 +1,40 @@
+import { useState } from "react"
+import { FaAngleUp, FaAngleDown } from "react-icons/fa";
+
+export default function FilterBar() {
+  const {open, setOpen} = useState(false)
+
+  return (
+    <section className="w-full mt-4 py-4 border-t border-b bg-gray-100">
+      <div className="block sm:hidden">
+        <a
+          href="/"
+          className="md:hidden text-base font-bold uppercase text-center flex justify-center items-center"
+        >
+          Blockchain Technology{" "}
+          {open ? (
+            <FaAngleDown className="ml-2" onClick={() => setOpen(false)} />
+          ) : (
+            <FaAngleUp className="ml-2" onClick={() => setOpen(true)} />
+          )}
+        </a>
+      </div>
+      <nav className="{open ? 'block' : 'hidden'} w-full flex-grow sm:flex sm:items-center sm:w-auto">
+        <div className="w-full container mx-auto flex flex-col sm:flex-row items-center justify-center text-sm font-bold uppercase mt-0 py-2">
+          <a href="/" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+            Public
+          </a>
+          <a href="/" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+            Private
+          </a>
+          <a href="/" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+            Hybrid
+          </a>
+          <a href="/" className="hover:bg-gray-400 rounded py-2 px-4 mx-2">
+            Consortium
+          </a>
+        </div>
+      </nav>
+    </section>
+  );
+}
