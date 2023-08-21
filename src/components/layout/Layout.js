eb3dff151e5795dbd94ada93e3604259a1cf1d55
John_Lamy <johnclamy@gmail.com>
2023-08-21 23:52:27 +0100

    add the FilterBar to Layout

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,11 +1,13 @@
 import Navbar from "./Navbar"
 import UserBar from "../../app/users/UserBar"
+import FilterBar from "./FilterBar"
 
 export default function Layout({ children }) {
   return (
     <div className="bg-white">
       <Navbar />
       <UserBar />
+      <FilterBar />
       {children}
     </div>
   )
