41bcff234ea265ef66a5b7e14d757a00c7c49963
John_Lamy <johnclamy@gmail.com>
2023-08-24 11:50:35 +0100

    remove the Filterbar component from Layout.js

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,13 +1,11 @@
 import Navbar from "./Navbar"
 import UserBar from "../../app/users/UserBar"
-import FilterBar from "./FilterBar"
 
 export default function Layout({ children }) {
   return (
     <div className="bg-white">
       <Navbar />
       <UserBar />
-      <FilterBar />
       {children}
     </div>
   )
