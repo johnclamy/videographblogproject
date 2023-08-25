55357a3cfd293e50cea9d6769a19f16bee21d8c9
John_Lamy <johnclamy@gmail.com>
2023-08-25 16:48:15 +0100

    replace the click event handler w/ onSetShowModal

--- src/components/widgets/AuthButton.js
+++ src/components/widgets/AuthButton.js
@@ -1,10 +1,7 @@
-export default function AuthButton({ onToggleAuth }) {
-  const handleClick = e => {
-    e.preventDefault()
-  }
+export default function AuthButton({ showModal, onSetShowModal }) {
   return (
     <button
-      onClick={handleClick}
+      onClick={() => onSetShowModal(!showModal)}
       className="relative inline-flex items-center justify-center p-0.5 ml-2 mt-4 overflow-hidden text-sm font-medium text-cyan-900 rounded-lg group bg-gradient-to-br from-cyan-600 to-cyan-800 group-hover:from-cyan-500 group-hover:to-gray-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
     >
       <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
