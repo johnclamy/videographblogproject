644cf2f30751ca7ff7a3cdd10d68a1d236a03e24
John_Lamy <johnclamy@gmail.com>
2023-08-25 16:36:48 +0100

    add a link to the register form

--- src/app/users/SigninForm.js
+++ src/app/users/SigninForm.js
@@ -1,14 +1,18 @@
 export default function SigninForm() {
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+      >
         <label htmlFor="username">Username:</label>
         <input type="text" name="username" id="username" />
         <label htmlFor="password">Password:</label>
         <input type="password" name="password" id="password" />
         <input type="submit" value="Sign In" />
+        <p>
+          Not registered yet? <a href="/">Register</a>
+        </p>
       </form>
-      <p>Not registered yet? <a href="/">Register</a></p>
     </div>
   );
 }
