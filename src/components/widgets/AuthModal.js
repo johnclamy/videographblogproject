b616869fb0e379d160b78c3bcced16b51d9815ac
John_Lamy <johnclamy@gmail.com>
2023-08-25 17:25:04 +0100

    close the modal by clicking anywhere in the window

--- src/components/widgets/AuthModal.js
+++ src/components/widgets/AuthModal.js
@@ -1,9 +1,11 @@
 export default function AuthModal({ isVisible, onSetShowModal }) {
+  const closeModal = () => onSetShowModal(false)
+
   const authModal = !isVisible ? null : (
-    <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
+    <div onClick={closeModal} className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
       <div className="w-full flex flex-col mx-2">
         <button
-          onClick={() => onSetShowModal(false)}
+          onClick={closeModal}
           className="text-white text-xl place-self-end"
         >
           X
