110bac228b424a89240efd773e4772cd9013f91f
John_Lamy <johnclamy@gmail.com>
2023-08-25 17:39:32 +0100

    add a target area for closing the modal

--- src/components/widgets/AuthModal.js
+++ src/components/widgets/AuthModal.js
@@ -1,8 +1,9 @@
 export default function AuthModal({ isVisible, onSetShowModal }) {
   const closeModal = () => onSetShowModal(false)
+  const handleCloseModal = e => e.target.id === 'modal-container' ? closeModal() : null
 
   const authModal = !isVisible ? null : (
-    <div onClick={closeModal} className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
+    <div id="modal-container" onClick={handleCloseModal} className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
       <div className="w-full flex flex-col mx-2">
         <button
           onClick={closeModal}
