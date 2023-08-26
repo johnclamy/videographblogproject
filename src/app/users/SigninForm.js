e55a74abd221d1bdced99ab3901b25a6f9707de6
John_Lamy <johnclamy@gmail.com>
2023-08-26 11:55:50 +0100

    setup the signin form

--- src/app/users/SigninForm.js
+++ src/app/users/SigninForm.js
@@ -1,18 +1,82 @@
 export default function SigninForm() {
   return (
-    <div>
-      <form
-        onSubmit={(e) => e.preventDefault()}
-      >
-        <label htmlFor="username">Username:</label>
-        <input type="text" name="username" id="username" />
-        <label htmlFor="password">Password:</label>
-        <input type="password" name="password" id="password" />
-        <input type="submit" value="Sign In" />
-        <p>
-          Not registered yet? <a href="/">Register</a>
+    <div className="flex min-h-full flex-col justify-center px-6 py-0 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={(e) => e.preventDefault()}>
+          <div>
+            <label
+              className="block text-sm font-medium leading-6 text-gray-900"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="username"
+                id="username"
+                required
+                className="block w-full rounded-md border-0 pv-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="text-sm">
+                <a
+                  href="/"
+                  className="font-semibold text-cyan-600 hover:text-cyan-500"
+                >
+                  Forgot password?
+                </a>
+              </div>
+            </div>
+
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Not registered yet?{" "}
+          <a
+            className="font-semibold leading-6 text-cyan-600 hover:text-cyan-500"
+            href="/"
+          >
+            Register
+          </a>
         </p>
-      </form>
+      </div>
     </div>
   );
 }
