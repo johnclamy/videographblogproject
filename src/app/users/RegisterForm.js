86b0ab9ff288975c5ffc61a1160dd0cdeee0faeb
John_Lamy <johnclamy@gmail.com>
2023-08-27 00:14:39 +0100

    setup the register form

--- src/app/users/RegisterForm.js
+++ src/app/users/RegisterForm.js
@@ -1,16 +1,94 @@
-export default function RegisterForm() {
+export default function RegisterForm({ onToggle }) {
   return (
-    <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <label htmlFor="username">Username:</label>
-        <input type="text" name="username" id="username" />
-        <label htmlFor="password">Password:</label>
-        <input type="password" name="password" id="password" />
-        <label htmlFor="password2">Confirm password:</label>
-        <input type="password2" name="password2" id="password2" />
-        <input type="submit" value="Sign Up" />
-      </form>
-      <p>Already registered? <a href="/">Sign in</a></p>
+    <div className="flex min-h-full flex-col justify-center px-6 py-0 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Register for an account
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form className="space-y-6" onSubmit={(e) => e.preventDefault()}>
+          <div>
+            <label
+              className="block text-sm font-medium leading-6 text-gray-900"
+              htmlFor="username"
+            >
+              Username
+            </label>
+            <div className="mt-2">
+              <input
+                type="text"
+                name="username"
+                id="username"
+                required
+                className="block w-full rounded-md border-0 pv-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-cyan-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+            </div>
+
+            <div className="mt-2">
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password2"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Confirm password
+              </label>
+            </div>
+
+            <div className="mt-2">
+              <input
+                id="password2"
+                name="password2"
+                type="password2"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+
+        <p className="mt-10 text-center text-sm text-gray-500">
+          Already registered?{" "}
+          <button
+            onClick={onToggle}
+            className="font-semibold leading-6 text-cyan-600 hover:text-cyan-500"
+          >
+            Sign in
+          </button>
+        </p>
+      </div>
     </div>
   );
 }
