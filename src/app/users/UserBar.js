8e68315016e148d9d97f83f06aea6572cf794bf4
John_Lamy <johnclamy@gmail.com>
2023-08-27 00:17:23 +0100

    toggle between register and sign in forms

--- src/app/users/UserBar.js
+++ src/app/users/UserBar.js
@@ -2,11 +2,15 @@ import { useState } from "react"
 import AuthButton from "../../components/widgets/AuthButton";
 import AuthModal from "../../components/widgets/AuthModal";
 import UserAuthDetails from "./UserAuthDetails";
+import RegisterForm from "./RegisterForm";
 import SigninForm from "./SigninForm";
 
 export default function UseBar() {
+  const [register, setRegister] = useState(false);
   const [signedIn, setSignedIn] = useState(false);
   const [showModal, setShowModal] = useState(false);
+
+  const toggleAuthForm = () => setRegister(!register)
 
   return (
     <section className="mx-3 mt-2">
@@ -16,7 +20,11 @@ export default function UseBar() {
         <UserAuthDetails onSignOut={() => setSignedIn(false)} />
       )}
       <AuthModal isVisible={showModal} onSetShowModal={setShowModal}>
-        <SigninForm />
+        {register ? (
+          <RegisterForm onToggle={toggleAuthForm} />
+        ) : (
+          <SigninForm onToggle={toggleAuthForm} />
+        )}
       </AuthModal>
     </section>
   );
