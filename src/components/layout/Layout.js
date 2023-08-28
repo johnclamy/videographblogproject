66f77f050c4874b7658401acdddb8853cb127d5d
John_Lamy <johnclamy@gmail.com>
2023-08-28 16:55:29 +0100

    add the app footer to the Layout.js

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar"
 import UserBar from "../../app/users/UserBar"
+import Footer from "./Footer";
 
 export default function Layout({ children }) {
   return (
@@ -7,6 +8,7 @@ export default function Layout({ children }) {
       <Navbar />
       <UserBar />
       {children}
+      <Footer />
     </div>
   )
 }
