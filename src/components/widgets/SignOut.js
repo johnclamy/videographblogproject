56f74cfa58af0a68086c2c00ef227b442cb20cd5
John_Lamy <johnclamy@gmail.com>
2023-08-29 15:18:27 +0100

    set auth btns as icons

--- src/components/widgets/SignOut.js
+++ src/components/widgets/SignOut.js
@@ -1,10 +1,12 @@
+import { FaSignOutAlt } from "react-icons/fa";
+
 export default function SignOut({ onSignOut }) {
   return (
     <button
       onClick={onSignOut}
       className="text-amber-200 hover:text-amber-300 hover:no-underline capitalize"
     >
-      Sign Out
+      <FaSignOutAlt />
     </button>
   );
 }
--- src/components/widgets/Signin.js
+++ src/components/widgets/Signin.js
@@ -1,7 +1,9 @@
+import { FaSignInAlt } from "react-icons/fa"
+
 export default function Signin({ onSignin }) {
   return (
     <button onClick={onSignin} className="text-amber-200 hover:text-amber-300 hover:no-underline capitalize mr-4">
-      sign in
+      <FaSignInAlt />
     </button>
   );
 }
