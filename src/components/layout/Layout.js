5c00257f6586c9c61cff4197ca855a42a3d29177
John_Lamy <johnclamy@gmail.com>
2023-08-29 14:29:56 +0100

    remove the UserBar and the modal

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,12 +1,13 @@
-import Navbar from "./Navbar"
-import UserBar from "../../app/users/UserBar"
+import { useState } from "react";
+import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export default function Layout({ children }) {
+  const [isSignedIn, setIsSignedIn] = useState(false)
+
   return (
-    <div className="bg-white">
-      <Navbar />
-      <UserBar />
+    <div className="bg-white h-full">
+      <Navbar isSignedIn={isSignedIn} onToggleSignin={() => setIsSignedIn(prev => !prev)} />
       {children}
       <Footer />
     </div>
