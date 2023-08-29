6123844a2709b0f806ea9e583a1c95b0247465a3
John_Lamy <johnclamy@gmail.com>
2023-08-29 22:35:56 +0100

    pass user obj as props to Layout.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import Layout from './components/layout/Layout';
 import Header from './components/layout/Header';
+import Post from './app/posts/Post';
+import user from './app/users/userAuthData' 
 import './App.css';
 
 function App() {
   return (
-    <Layout>
+    <Layout user={user}>
       <Header />
+      <main className='container bg-slate-100 mx-auto flex flex-wrap py-4'>
+        <Post />
+      </main>
     </Layout>
   );
 }
