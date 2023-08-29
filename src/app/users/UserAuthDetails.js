0017ca390a37088e35418c88b0a226035f4f2a29
John_Lamy <johnclamy@gmail.com>
2023-08-29 15:19:19 +0100

    change user info font to normal

--- src/app/users/UserAuthDetails.js
+++ src/app/users/UserAuthDetails.js
@@ -2,8 +2,8 @@ import SignOut from "../../components/widgets/SignOut";
 
 export default function UserAuthDetails({ onSignOut }) {
   return (
-    <p className="text-white flex gap-2 pr-4">
-      <b>John Doe</b>
+    <p className="text-white flex justify-start items-center gap-2 pr-4">
+      <b className="text-sm font-normal">John Doe</b>
       <SignOut onSignOut={onSignOut} />
     </p>
   );
