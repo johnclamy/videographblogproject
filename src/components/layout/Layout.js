8750ed69b0af57e8b38e45319eacbd825928b3c5
John_Lamy <johnclamy@gmail.com>
2023-08-30 11:50:10 +0100

    pass down userIsSignedIn prop boolean value

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,14 +2,17 @@ import { useState } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-export default function Layout({ children, user }) {
-  const [isSignedIn, setIsSignedIn] = useState(user.isSignedIn)
+export default function Layout({ children, userIsSignedIn }) {
+  const [isSignedIn, setIsSignedIn] = useState(userIsSignedIn);
 
   return (
     <div className="bg-white h-full">
-      <Navbar isSignedIn={isSignedIn} onToggleSignin={() => setIsSignedIn(prev => !prev)} />
+      <Navbar
+        isSignedIn={isSignedIn}
+        onToggleSignin={() => setIsSignedIn((prev) => !prev)}
+      />
       {children}
       <Footer />
     </div>
-  )
+  );
 }
