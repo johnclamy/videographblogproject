252582bb07e35984a6cf96860f884d6f8ddf132f
John_Lamy <johnclamy@gmail.com>
2023-08-30 11:45:44 +0100

    add blog obj to dummy user data

--- src/app/users/userAuthData.js
+++ src/app/users/userAuthData.js
@@ -1,6 +1,19 @@
 const user = {
-  name: 'John Doe',
+  name: "John Doe",
   isSignedIn: false,
-}
+  blogs: [
+    {
+      description: "Role set leader structure.",
+      id: 19,
+      content:
+        "Money form live strategy consider finally. Must time lose trade force will usually.",
+      category: "love",
+      updatedAt: "2023-03-16T19:06:12.184272",
+      title: "Defense the travel.",
+      photo: "https://api.slingacademy.com/public/sample-blog-posts/1.png",
+      createdAt: "2023-03-16T19:06:12.184269",
+    },
+  ],
+};
 
 export default user
