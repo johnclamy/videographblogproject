43ea0f8597461b8fe65a18184801926c37a0fcc3
John_Lamy <johnclamy@gmail.com>
2023-08-30 14:57:04 +0100

    format the post date

--- src/app/posts/Post.js
+++ src/app/posts/Post.js
@@ -1,14 +1,16 @@
 import { FaArrowRight } from "react-icons/fa";
 
 export default function Post({ username, post }) {
+  const date = new Date(post.createdAt)
+  const renderDate = `${date.getDate()}/${
+    (date.getMonth() + 1)
+  }/${date.getFullYear()}`;
+
   return (
     <section className="w-full md:w-2/3 flex flex-col items-center px-3">
       <div className="flex flex-col shadow my-4">
         <a href="/" className="hover:opacity-75">
-          <img
-            alt={post.description}
-            src={post.photo}
-          />
+          <img alt={post.description} src={post.photo} />
         </a>
         <section className="bg-white flex flex-col justify-start p-6">
           <a
@@ -17,18 +19,18 @@ export default function Post({ username, post }) {
           >
             {post.category}
           </a>
-          <a href="/" class="text-3xl font-bold hover:text-gray-700 pb-4">
+          <a href="/" className="text-3xl font-bold hover:text-gray-700 pb-4">
             {post.title}
           </a>
-          <p href="/" class="text-sm pb-3">
-            By{" "}
-            <a href="/" class="font-semibold hover:text-gray-800">
+          <p className="text-sm pb-3">
+            Posted by{" "}
+            <a href="/" className="font-semibold hover:text-gray-800">
               {username}
             </a>
-            , <i>August 24th, 2023</i>
+            , <i>{renderDate}</i>
           </p>
           <a href="/" className="pb-6">
-            {post.content}
+            {post.content}...
           </a>
           <a href="/" class="uppercase text-cyan-800 hover:text-cyan-900">
             Continue Reading <FaArrowRight className="inline-block" />
