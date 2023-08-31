ac2cbefc6001694232c80f59dee01090d2eded83
John_Lamy <johnclamy@gmail.com>
2023-08-31 16:28:17 +0100

    add CreatePost.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import Layout from './components/layout/Layout';
 import Header from './components/layout/Header';
+import CreatePost from './app/posts/CreatePost';
 import Post from './app/posts/Post';
-import user from './app/users/userAuthData' 
+import user from './app/users/userAuthData'
 import './App.css';
 
 function App() {
+  const { name } = user
+  const post = user.blogs[1]
+
   return (
     <Layout userIsSignedIn={user.isSignedIn}>
       <Header />
-      <main className='container bg-slate-100 mx-auto flex flex-wrap py-4'>
-        <Post username={user.name} post={user.blogs[0]} />
+      <main className="container bg-slate-100 mx-auto flex flex-wrap py-4">
+        {name ? <CreatePost username={name} /> : null}
+        {!post ? (
+          <p className=" w-full md:w-3/5 lg:w-7/12 mx-auto bg-cyan-600 p-3 text-center text-white text-lg">
+            <strong>Sorry</strong>! Currently no posts available for viewing.
+          </p>
+        ) : (
+          <Post username={name} post={post} />
+        )}
       </main>
     </Layout>
   );
