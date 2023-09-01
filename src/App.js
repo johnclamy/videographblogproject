d5e86fac9ffe216cd9ece44e7fcae6f64108595e
John_Lamy <johnclamy@gmail.com>
2023-09-01 10:47:00 +0100

    move user state to the App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import Layout from './components/layout/Layout';
-import Header from './components/layout/Header';
-import CreatePost from './app/posts/CreatePost';
-import Post from './app/posts/Post';
-import user from './app/users/userAuthData'
-import './App.css';
+import { useState } from "react";
+import Layout from "./components/layout/Layout";
+import Header from "./components/layout/Header";
+import CreatePost from "./app/posts/CreatePost";
+import Post from "./app/posts/Post";
+import user from "./app/users/userAuthData";
+import "./App.css";
 
 function App() {
-  const { name } = user
-  const post = user.blogs[1]
+  const [username, setUsername] = useState(user.name)
+  const [isSignedIn, setIsSignedIn] = useState(user.isSignedIn)
+  const [post, setPost] = useState(user.blogs[0])
+
+  const handleToggleSignin = () => setIsSignedIn((prev) => !prev);
 
   return (
-    <Layout userIsSignedIn={user.isSignedIn}>
+    <Layout userIsSignedIn={isSignedIn} onToggleSignin={handleToggleSignin}>
       <Header />
       <main className="container bg-slate-100 mx-auto flex flex-wrap py-4">
-        {name ? <CreatePost username={name} /> : null}
+        {isSignedIn ? <CreatePost username={username} /> : null}
         {!post ? (
           <p className=" w-full md:w-3/5 lg:w-7/12 mx-auto bg-cyan-600 p-3 text-center text-white text-lg">
             <strong>Sorry</strong>! Currently no posts available for viewing.
           </p>
         ) : (
-          <Post username={name} post={post} />
+          <Post username={username} post={post} />
         )}
       </main>
     </Layout>
