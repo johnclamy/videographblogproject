d169181d7838cc775ebe586b8e919f7fb35ab221
John_Lamy <johnclamy@gmail.com>
2024-05-30 01:57:18 +0100

    define the app colours in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        appGrey: {
+          200: "#E8EDDF",
+          300: "#CFDBD5",
+          800: "#333533",
+          900: "#242423",
+        },
+        appYellow: {
+          200: "#F5CB5C",
+        },
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
