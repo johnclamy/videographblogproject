29e61e616d7d4c453f3872182325dbcf77333c88
johnclamy <johnclamy@gmail.com>
2025-09-21 10:51:23 +0100

    configure the about url for the default app

--- default/urls.py
+++ default/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home_page
+from .views import home_page, about_page
 
 
 urlpatterns = [
     path('', home_page, name='default.home_page'),
+    path('about', about_page, name='default.about_page'),
 ]
 
