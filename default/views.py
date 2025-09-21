1bb99650c85ed3d4da2afee5b95566e599b22351
johnclamy <johnclamy@gmail.com>
2025-09-21 10:59:54 +0100

    define the about function of the default app

--- default/views.py
+++ default/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'default/index.html')
+
+
+def about_page(request):
+    return render(request, 'default/about_page.html')
