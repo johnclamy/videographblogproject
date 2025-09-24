1474621535705015f7a82b112d8be241c5bd5924
johnclamy <johnclamy@gmail.com>
2025-09-24 09:18:10 +0100

    pass the title from the view home_page function to the template

--- default/views.py
+++ default/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'default/index.html')
+    data = {}
+    data['page_title'] = 'Welcome to my Videograph blog | Home Page'
+    return render(request, 'default/index.html', {'data': data})
 
 
 def about_page(request):
